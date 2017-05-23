9a8be855e88435feb932fc494e995e81b9dfdf97
odashi <yus.takara@gmail.com>
2017-05-23 22:24:19 +0900

    updated XOR example.

--- test/example_xor_test.cc
+++ test/example_xor_test.cc
@@ -1,9 +1,10 @@
 #include <config.h>
 
+#include <iostream>
 #include <gtest/gtest.h>
-
 #include <primitiv/cpu_device.h>
 #include <primitiv/node_ops.h>
+#include <primitiv/sgd_trainer.h>
 
 namespace primitiv {
 
@@ -16,32 +17,55 @@ TEST(ExampleTest, Xor) {
   CPUDevice dev;
 
   // Parameters
-  Parameter pw1({2, 2}, &dev);
-  Parameter pb1({2}, &dev);
-  Parameter pw2({1, 2}, &dev);
+  Parameter pw1({4, 2}, &dev);
+  Parameter pb1({4}, &dev);
+  Parameter pw2({1, 4}, &dev);
   Parameter pb2({}, &dev);
-  pw1.reset_value({1, -1, 1, -1});
-  pb1.reset_value({-1, -1});
-  pw2.reset_value({1, 1});
-  pb2.reset_value({1});
+  pw1.reset_value({1, 1, -1, -1, 1, -1, 1, -1});
+  pb1.reset_value({0, 0, 0, 0});
+  pw2.reset_value({1, -1, -1, 1});
+  pb2.reset_value({0});
 
-  // Build computation graph.
-  Graph g;
-  Node x = F::input(g, dev, Shape({2}, 4), {1, 1, 1, -1, -1, 1, -1, -1});
-  Node w1 = F::parameter(g, pw1);
-  Node b1 = F::parameter(g, pb1);
-  Node w2 = F::parameter(g, pw2);
-  Node b2 = F::parameter(g, pb2);
-  
-  Node h = F::tanh(F::dot(w1, x) + b1);
-  Node y = F::dot(w2, h) + b2;
+  // Trainer
+  SGDTrainer trainer(.1);
+  trainer.add_parameter(&pw1);
+  trainer.add_parameter(&pb1);
+  trainer.add_parameter(&pw2);
+  trainer.add_parameter(&pb2);
 
-  // Get result.
-  std::vector<float> y_val = g.forward(y).get_values();
-  EXPECT_FLOAT_EQ(.76653940, y_val[0]);
-  EXPECT_FLOAT_EQ(-.52318831, y_val[1]);
-  EXPECT_FLOAT_EQ(-.52318831, y_val[2]);
-  EXPECT_FLOAT_EQ(.76653940, y_val[3]);
+  float prev_loss = 1e10;
+
+  // Training loop.
+  for (unsigned i = 0; i < 10; ++i) {
+    // Build computation graph.
+    Graph g;
+    Node x = F::input(g, dev, Shape({2}, 4), {1, 1, 1, -1, -1, 1, -1, -1});
+    Node w1 = F::parameter(g, pw1);
+    Node b1 = F::parameter(g, pb1);
+    Node w2 = F::parameter(g, pw2);
+    Node b2 = F::parameter(g, pb2);
+    Node h = F::tanh(F::dot(w1, x) + b1);
+    Node y = F::dot(w2, h) + b2;
+    Node t = F::input(g, dev, Shape({}, 4), {1, -1, -1, 1});
+    Node diff = t - y;
+    Node loss = diff * diff;
+
+    // Get result.
+    std::vector<float> y_val = g.forward(y).get_values();
+    std::cout << "epoch " << i << ":" << std::endl;
+    for (unsigned j = 0; j < 4; ++j) {
+      std::cout << "  [" << j << "]: " << y_val[j] << std::endl;
+    }
+
+    // Backpropagation
+    trainer.reset_gradients();
+    float loss_val = g.forward(loss).get_values()[0];
+    std::cout << "  loss: " << loss_val << std::endl;
+    EXPECT_LT(loss_val, prev_loss);
+    prev_loss = loss_val;
+    g.backward(loss);
+    trainer.update();
+  }
 }
 
 }  // namespace primitiv
