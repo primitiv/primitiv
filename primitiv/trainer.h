149bf90195ee994b77d8d329a9b0400126836eaa
Yusuke Oda <yus.takara@gmail.com>
2017-05-23 11:57:17 +0900

    add Trainer interface.

---
+++ primitiv/trainer.h
@@ -0,0 +1,40 @@
+#ifndef PRIMITIV_TRAINER_H_
+#define PRIMITIV_TRAINER_H_
+
+namespace primitiv {
+
+class Parameter;
+
+/**
+ * Abstract class for parameter optimizers.
+ */
+class Trainer {
+  Trainer(const Trainer &) = delete;
+  Trainer(Trainer &&) = delete;
+  Trainer &operator=(const Trainer &) = delete;
+  Trainer &operator=(Trainer &&) = delete;
+
+public:
+  Trainer() = default;
+  virtual ~Trainer() = default;
+
+  /**
+   * Registers a parameter.
+   * @param param Parameter to be optimized.
+   */
+  virtual void add_parameter(Parameter *param) = 0;
+
+  /**
+   * Resets all gradients of registered parameters.
+   */
+  virtual void reset_gradients() = 0;
+
+  /**
+   * Updates parameter values.
+   */
+  virtual void update() = 0;
+};
+
+}  // namespace primitiv
+
+#endif  // PRIMITIV_TRAINER_H_
