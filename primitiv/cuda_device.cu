4228a175ac2bf12a71974cf4ce84611e083fbe27
odashi <yus.takara@gmail.com>
2017-05-26 23:10:58 +0900

    add some ops for CUDA

--- primitiv/cuda_device.cu
+++ primitiv/cuda_device.cu
@@ -1,9 +1,5 @@
 #include <config.h>
 
-#include <algorithm>
-#include <cstdlib>
-#include <cstring>
-#include <cmath>
 #include <iostream>
 #include <sstream>
 #include <stdexcept>
@@ -29,28 +25,80 @@ namespace {
  * CUDA kernels
  */
 
-__global__ void cuda_set_const(float *ptr, const float k, const unsigned size) {
-  const unsigned i = threadIdx.x + blockIdx.x * blockDim.x;
-  if (i < size) ptr[i] = k;
+#define IDX threadIdx.x + blockIdx.x * blockDim.x
+
+__global__ void dev_set_const(float *py, const float k, const unsigned size) {
+  const unsigned i = IDX;
+  if (i < size) py[i] = k;
 }
 
-__global__ void cuda_math_exp(
-    float *dest, const float *src, const unsigned size) {
-  const unsigned i = threadIdx.x + blockIdx.x * blockDim.x;
-  if (i < size) dest[i] = ::exp(src[i]);
+__global__ void dev_negate(float *py, const float *px, const unsigned size) {
+  const unsigned i = IDX;
+  if (i < size) py[i] = -px[i];
 }
 
-__global__ void cuda_math_tanh(
-    float *dest, const float *src, const unsigned size) {
-  const unsigned i = threadIdx.x + blockIdx.x * blockDim.x;
-  if (i < size) dest[i] = ::tanh(src[i]);
+__global__ void dev_add_const(
+    float *py, const float *px, const float k, const unsigned size) {
+  const unsigned i = IDX;
+  if (i < size) py[i] = px[i] + k;
 }
 
-__global__ void cuda_math_sigmoid(
-    float *dest, const float *src, const unsigned size) {
-  const unsigned i = threadIdx.x + blockIdx.x * blockDim.x;
-  if (i < size) dest[i] = .5f + .5f * ::tanh(.5f * src[i]);
+__global__ void dev_subtract_const_l(
+    float *py, const float *px, const float k, const unsigned size) {
+  const unsigned i = IDX;
+  if (i < size) py[i] = k - px[i];
 }
+
+__global__ void dev_subtract_const_r(
+    float *py, const float *px, const float k, const unsigned size) {
+  const unsigned i = IDX;
+  if (i < size) py[i] = px[i] - k;
+}
+
+__global__ void dev_multiply_const(
+    float *py, const float *px, const float k, const unsigned size) {
+  const unsigned i = IDX;
+  if (i < size) py[i] = px[i] * k;
+}
+
+__global__ void dev_divide_const_l(
+    float *py, const float *px, const float k, const unsigned size) {
+  const unsigned i = IDX;
+  if (i < size) py[i] = k / px[i];
+}
+
+__global__ void dev_divide_const_r(
+    float *py, const float *px, const float k, const unsigned size) {
+  const unsigned i = IDX;
+  if (i < size) py[i] = px[i] / k;
+}
+
+__global__ void dev_exp(float *py, const float *px, const unsigned size) {
+  const unsigned i = IDX;
+  if (i < size) py[i] = ::expf(px[i]);
+}
+
+__global__ void dev_tanh(float *py, const float *px, const unsigned size) {
+  const unsigned i = IDX;
+  if (i < size) py[i] = ::tanhf(px[i]);
+}
+
+__global__ void dev_sigmoid(float *py, const float *px, const unsigned size) {
+  const unsigned i = IDX;
+  if (i < size) py[i] = .5f + .5f * ::tanhf(.5f * px[i]);
+}
+
+__global__ void dev_step(float *py, const float *px, const unsigned size) {
+  const unsigned i = IDX;
+  if (i < size) py[i] = (float)(px[i] >= .0f);
+}
+
+__global__ void dev_relu(float *py, const float *px, const unsigned size) {
+  const unsigned i = IDX;
+  if (i < size) py[i] = ::fmaxf(px[i], .0f);
+}
+
+#undef IDX
 
 }  // namespace
 
@@ -152,7 +200,7 @@ void CUDADevice::reset_tensor(Tensor &x, const float k) {
   CHECK_DEVICE(x);
   const unsigned num_elements = x.shape().size();
   const unsigned num_blocks = (num_elements + max_threads_ - 1) / max_threads_;
-  ::cuda_set_const<<<num_blocks, max_threads_>>>(DATA(x), k, num_elements);
+  ::dev_set_const<<<num_blocks, max_threads_>>>(DATA(x), k, num_elements);
 }
 
 void CUDADevice::reset_tensor(Tensor &x, const std::vector<float> &values) {
@@ -172,32 +220,64 @@ void CUDADevice::reset_tensor(Tensor &x, const std::vector<float> &values) {
 
 Tensor CUDADevice::duplicate(const Tensor &x) {
   CHECK_DEVICE(x);
-  throw std::runtime_error("not implemented.");
+  Tensor ret = new_tensor(x.shape());
+  ::cudaMemcpy(
+      ret.data(), x.data(), sizeof(float) * x.shape().size(),
+      cudaMemcpyDeviceToDevice);
+  return ret;
 }
 
-Tensor CUDADevice::negate(const Tensor &x) {
-  CHECK_DEVICE(x);
-  throw std::runtime_error("not implemented.");
+#define CUDA_DEV_UNARY(name, kernel) \
+Tensor CUDADevice::name(const Tensor &x) { \
+  CHECK_DEVICE(x); \
+  Tensor ret = new_tensor(x.shape()); \
+  const unsigned size = x.shape().size(); \
+  const unsigned num_blocks = (size + max_threads_ - 1) / max_threads_; \
+  ::kernel<<<num_blocks, max_threads_>>>(DATA(ret), CDATA(x), size); \
+  return ret; \
 }
 
-Tensor CUDADevice::add(const Tensor &x, const float k) {
-  CHECK_DEVICE(x);
-  throw std::runtime_error("not implemented.");
+#define CUDA_DEV_BINARY_KX(name, kernel) \
+Tensor CUDADevice::name(const float k, const Tensor &x) { \
+  CHECK_DEVICE(x); \
+  Tensor ret = new_tensor(x.shape()); \
+  const unsigned size = x.shape().size(); \
+  const unsigned num_blocks = (size + max_threads_ - 1) / max_threads_; \
+  ::kernel<<<num_blocks, max_threads_>>>(DATA(ret), CDATA(x), k, size); \
+  return ret; \
 }
+
+#define CUDA_DEV_BINARY_XK(name, kernel) \
+Tensor CUDADevice::name(const Tensor &x, const float k) { \
+  CHECK_DEVICE(x); \
+  Tensor ret = new_tensor(x.shape()); \
+  const unsigned size = x.shape().size(); \
+  const unsigned num_blocks = (size + max_threads_ - 1) / max_threads_; \
+  ::kernel<<<num_blocks, max_threads_>>>(DATA(ret), CDATA(x), k, size); \
+  return ret; \
+}
+
+CUDA_DEV_UNARY(negate, dev_negate);
+CUDA_DEV_UNARY(exp, dev_exp);
+CUDA_DEV_UNARY(tanh, dev_tanh);
+CUDA_DEV_UNARY(sigmoid, dev_sigmoid);
+CUDA_DEV_UNARY(step, dev_step);
+CUDA_DEV_UNARY(relu, dev_relu);
+
+CUDA_DEV_BINARY_XK(add, dev_add_const);
+CUDA_DEV_BINARY_KX(subtract, dev_subtract_const_l);
+CUDA_DEV_BINARY_XK(subtract, dev_subtract_const_r);
+CUDA_DEV_BINARY_XK(multiply, dev_multiply_const);
+CUDA_DEV_BINARY_KX(divide, dev_divide_const_l);
+CUDA_DEV_BINARY_XK(divide, dev_divide_const_r);
+
+#undef CUDA_DEV_UNARY
+#undef CUDA_DEV_BINARY_KX
+#undef CUDA_DEV_BINARY_XK
 
 Tensor CUDADevice::add(const Tensor &a, const Tensor &b) {
   CHECK_DEVICE(a);
   CHECK_DEVICE(b);
-  throw std::runtime_error("not implemented.");
-}
-
-Tensor CUDADevice::subtract(const Tensor &x, const float k) {
-  CHECK_DEVICE(x);
-  throw std::runtime_error("not implemented.");
-}
-
-Tensor CUDADevice::subtract(const float k, const Tensor &x) {
-  CHECK_DEVICE(x);
   throw std::runtime_error("not implemented.");
 }
 
@@ -207,24 +287,9 @@ Tensor CUDADevice::subtract(const Tensor &a, const Tensor &b) {
   throw std::runtime_error("not implemented.");
 }
 
-Tensor CUDADevice::multiply(const Tensor &x, const float k) {
-  CHECK_DEVICE(x);
-  throw std::runtime_error("not implemented.");
-}
-
 Tensor CUDADevice::multiply(const Tensor &a, const Tensor &b) {
   CHECK_DEVICE(a);
   CHECK_DEVICE(b);
-  throw std::runtime_error("not implemented.");
-}
-
-Tensor CUDADevice::divide(const Tensor &x, const float k) {
-  CHECK_DEVICE(x);
-  throw std::runtime_error("not implemented.");
-}
-
-Tensor CUDADevice::divide(const float k, const Tensor &x) {
-  CHECK_DEVICE(x);
   throw std::runtime_error("not implemented.");
 }
 
@@ -248,49 +313,6 @@ Tensor CUDADevice::transpose(const Tensor &x) {
 Tensor CUDADevice::dot(const Tensor &a, const Tensor &b) {
   CHECK_DEVICE(a);
   CHECK_DEVICE(b);
-  throw std::runtime_error("not implemented.");
-}
-
-Tensor CUDADevice::exp(const Tensor &x) {
-  CHECK_DEVICE(x);
-
-  Tensor ret = new_tensor(x.shape());
-  const unsigned num_elements = x.shape().size();
-  const unsigned num_blocks = (num_elements + max_threads_ - 1) / max_threads_;
-  ::cuda_math_exp<<<num_blocks, max_threads_>>>(
-      DATA(ret), CDATA(x), num_elements);
-  return ret;
-}
-
-Tensor CUDADevice::tanh(const Tensor &x) {
-  CHECK_DEVICE(x);
-
-  Tensor ret = new_tensor(x.shape());
-  const unsigned num_elements = x.shape().size();
-  const unsigned num_blocks = (num_elements + max_threads_ - 1) / max_threads_;
-  ::cuda_math_tanh<<<num_blocks, max_threads_>>>(
-      DATA(ret), CDATA(x), num_elements);
-  return ret;
-}
-
-Tensor CUDADevice::sigmoid(const Tensor &x) {
-  CHECK_DEVICE(x);
-
-  Tensor ret = new_tensor(x.shape());
-  const unsigned num_elements = x.shape().size();
-  const unsigned num_blocks = (num_elements + max_threads_ - 1) / max_threads_;
-  ::cuda_math_sigmoid<<<num_blocks, max_threads_>>>(
-      DATA(ret), CDATA(x), num_elements);
-  return ret;
-}
-
-Tensor CUDADevice::step(const Tensor &x) {
-  CHECK_DEVICE(x);
-  throw std::runtime_error("not implemented.");
-}
-
-Tensor CUDADevice::relu(const Tensor &x) {
-  CHECK_DEVICE(x);
   throw std::runtime_error("not implemented.");
 }
 
