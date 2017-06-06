720e6177482c18bcc6439be422295197277677b8
odashi <yus.takara@gmail.com>
2017-06-07 00:50:31 +0900

    separated node_ops implementations.

---
+++ primitiv/node_ops.cc
@@ -0,0 +1,108 @@
+#include <config.h>
+
+#include <vector>
+#include <primitiv/function_impl.h>
+#include <primitiv/graph.h>
+#include <primitiv/node.h>
+#include <primitiv/shape.h>
+#include <primitiv/parameter.h>
+
+namespace primitiv {
+
+Node operator+(const Node &x) {
+  return x.graph()->add_function(new functions::Positive(), {x});
+}
+
+Node operator-(const Node &x) {
+  return x.graph()->add_function(new functions::Negative(), {x});
+}
+
+Node operator+(const Node &x, const float k) {
+  return x.graph()->add_function(new functions::AddConst(k), {x});
+}
+
+Node operator+(const float k, const Node &x) {
+  return x.graph()->add_function(new functions::AddConst(k), {x});
+}
+
+Node operator+(const Node &a, const Node &b) {
+  return a.graph()->add_function(new functions::Add(), {a, b});
+}
+
+Node operator-(const Node &x, const float k) {
+  return x.graph()->add_function(new functions::SubtractConstR(k), {x});
+}
+
+Node operator-(const float k, const Node &x) {
+  return x.graph()->add_function(new functions::SubtractConstL(k), {x});
+}
+
+Node operator-(const Node &a, const Node &b) {
+  return a.graph()->add_function(new functions::Subtract(), {a, b});
+}
+
+Node operator*(const Node &x, const float k) {
+  return x.graph()->add_function(new functions::MultiplyConst(k), {x});
+}
+
+Node operator*(const float k, const Node &x) {
+  return x.graph()->add_function(new functions::MultiplyConst(k), {x});
+}
+
+Node operator*(const Node &a, const Node &b) {
+  return a.graph()->add_function(new functions::Multiply(), {a, b});
+}
+
+Node operator/(const Node &x, const float k) {
+  return x.graph()->add_function(new functions::DivideConstR(k), {x});
+}
+
+Node operator/(const float k, const Node &x) {
+  return x.graph()->add_function(new functions::DivideConstL(k), {x});
+}
+
+Node operator/(const Node &a, const Node &b) {
+  return a.graph()->add_function(new functions::Divide(), {a, b});
+}
+
+namespace node_ops {
+
+Node input(
+    Graph *g, Device *dev, const Shape &shape, const std::vector<float> &data) {
+  return g->add_function(new functions::Input(shape, dev, data), {});
+}
+
+Node parameter(Graph *g, Parameter *param) {
+  return g->add_function(new functions::ParameterInput(param), {});
+}
+
+Node transpose(const Node &x) {
+  return x.graph()->add_function(new functions::Transpose(), {x});
+}
+
+Node dot(const Node &a, const Node &b) {
+  return a.graph()->add_function(new functions::Dot(), {a, b});
+}
+
+Node exp(const Node &x) {
+  return x.graph()->add_function(new functions::Exp(), {x});
+}
+
+Node tanh(const Node &x) {
+  return x.graph()->add_function(new functions::Tanh(), {x});
+}
+
+Node sigmoid(const Node &x) {
+  return x.graph()->add_function(new functions::Sigmoid(), {x});
+}
+
+Node relu(const Node &x) {
+  return x.graph()->add_function(new functions::ReLU(), {x});
+}
+
+Node batch_sum(const Node &x) {
+  return x.graph()->add_function(new functions::BatchSum(), {x});
+}
+
+}  // namespace node_ops
+}  // namespace primitiv
--- primitiv/node_ops.h
+++ primitiv/node_ops.h
@@ -2,59 +2,46 @@
 #define PRIMITIV_NODE_OPS_H_
 
 #include <vector>
-#include <primitiv/function_impl.h>
-#include <primitiv/graph.h>
 #include <primitiv/node.h>
-#include <primitiv/shape.h>
-#include <primitiv/parameter.h>
-
-#define APP(x) (x).graph()->add_function
-#define F functions
 
 namespace primitiv {
 
-inline Node operator+(const Node &x) { return APP(x)(new F::Positive(), {x}); }
-inline Node operator-(const Node &x) { return APP(x)(new F::Negative(), {x}); }
-inline Node operator+(const Node &x, const float k) { return APP(x)(new F::AddConst(k), {x}); }
-inline Node operator+(const float k, const Node &x) { return APP(x)(new F::AddConst(k), {x}); }
-inline Node operator+(const Node &a, const Node &b) { return APP(a)(new F::Add(), {a, b}); }
-inline Node operator-(const Node &x, const float k) { return APP(x)(new F::SubtractConstR(k), {x}); }
-inline Node operator-(const float k, const Node &x) { return APP(x)(new F::SubtractConstL(k), {x}); }
-inline Node operator-(const Node &a, const Node &b) { return APP(a)(new F::Subtract(), {a, b}); }
-inline Node operator*(const Node &x, const float k) { return APP(x)(new F::MultiplyConst(k), {x}); }
-inline Node operator*(const float k, const Node &x) { return APP(x)(new F::MultiplyConst(k), {x}); }
-inline Node operator*(const Node &a, const Node &b) { return APP(a)(new F::Multiply(), {a, b}); }
-inline Node operator/(const Node &x, const float k) { return APP(x)(new F::DivideConstR(k), {x}); }
-inline Node operator/(const float k, const Node &x) { return APP(x)(new F::DivideConstL(k), {x}); }
-inline Node operator/(const Node &a, const Node &b) { return APP(a)(new F::Divide(), {a, b}); }
+class Device;
+class Graph;
+class Parameter;
+
+Node operator+(const Node &x);
+Node operator-(const Node &x);
+Node operator+(const Node &x, float k);
+Node operator+(float k, const Node &x);
+Node operator+(const Node &a, const Node &b);
+Node operator-(const Node &x, float k);
+Node operator-(float k, const Node &x);
+Node operator-(const Node &a, const Node &b);
+Node operator*(const Node &x, float k);
+Node operator*(float k, const Node &x);
+Node operator*(const Node &a, const Node &b);
+Node operator/(const Node &x, float k);
+Node operator/(float k, const Node &x);
+Node operator/(const Node &a, const Node &b);
 
 namespace node_ops {
 
-inline Node input(
-    Graph *g,
-    Device *dev,
-    const Shape &shape,
-    const std::vector<float> &data) {
-  return g->add_function(new F::Input(shape, dev, data), {});
-}
+Node input(
+    Graph *g, Device *dev, const Shape &shape, const std::vector<float> &data);
 
-inline Node parameter(Graph *g, Parameter *param) {
-  return g->add_function(new F::ParameterInput(param), {});
-}
+Node parameter(Graph *g, Parameter *param);
 
-inline Node transpose(const Node &x) { return APP(x)(new F::Transpose(), {x}); }
-inline Node dot(const Node &a, const Node &b) { return APP(a)(new F::Dot(), {a, b}); }
-inline Node exp(const Node &x) { return APP(x)(new F::Exp(), {x}); }
-inline Node tanh(const Node &x) { return APP(x)(new F::Tanh(), {x}); }
-inline Node sigmoid(const Node &x) { return APP(x)(new F::Sigmoid(), {x}); }
-inline Node relu(const Node &x) { return APP(x)(new F::ReLU(), {x}); }
+Node transpose(const Node &x);
+Node dot(const Node &a, const Node &b);
+Node exp(const Node &x);
+Node tanh(const Node &x);
+Node sigmoid(const Node &x);
+Node relu(const Node &x);
 
-inline Node batch_sum(const Node &x) { return APP(x)(new F::BatchSum(), {x}); }
+Node batch_sum(const Node &x);
 
 }  // namespace node_ops
 }  // namespace primitiv
-
-#undef APP
-#undef F
 
 #endif  // PRIMITIV_NODE_OPS_H_
