53e99087b952c63907ac1af07b12bcd1c8394c88
odashi <yus.takara@gmail.com>
2017-06-06 23:02:37 +0900

    separated tensor_ops implementations.

---
+++ primitiv/tensor_ops.cc
@@ -0,0 +1,107 @@
+#include <config.h>
+
+#include <primitiv/device.h>
+#include <primitiv/tensor_ops.h>
+
+namespace primitiv {
+
+Tensor operator+(const Tensor &x) {
+  return x.device()->duplicate(x);
+}
+
+Tensor operator-(const Tensor &x) {
+  return x.device()->negate(x);\
+}
+
+Tensor operator+(const Tensor &x, float k) {
+  return x.device()->add(x, k);
+}
+
+Tensor operator+(float k, const Tensor &x) {
+  return x.device()->add(x, k);
+}
+
+Tensor operator+(const Tensor &a, const Tensor &b) {
+  return a.device()->add(a, b);
+}
+
+Tensor operator-(const Tensor &x, float k) {
+  return x.device()->subtract(x, k);
+}
+
+Tensor operator-(float k, const Tensor &x) {
+  return x.device()->subtract(k, x);
+}
+
+Tensor operator-(const Tensor &a, const Tensor &b) {
+  return a.device()->subtract(a, b);
+}
+
+Tensor operator*(const Tensor &x, float k) {
+  return x.device()->multiply(x, k);
+}
+
+Tensor operator*(float k, const Tensor &x) {
+  return x.device()->multiply(x, k);
+}
+
+Tensor operator*(const Tensor &a, const Tensor &b) {
+  return a.device()->multiply(a, b);
+}
+
+Tensor operator/(const Tensor &x, float k) {
+  return x.device()->divide(x, k);
+}
+
+Tensor operator/(float k, const Tensor &x) {
+  return x.device()->divide(k, x);
+}
+
+Tensor operator/(const Tensor &a, const Tensor &b) {
+  return a.device()->divide(a, b);
+}
+
+namespace tensor_ops {
+
+Tensor slice(const Tensor &x, unsigned dim, unsigned lower, unsigned upper) {
+  return x.device()->slice(x, dim, lower, upper);
+}
+Tensor concat(const std::vector<const Tensor *> &xs, unsigned dim) {
+  if (xs.empty()) THROW_ERROR("No tensors to be concatenated.");
+  return xs[0]->device()->concat(xs, dim);
+}
+
+Tensor transpose(const Tensor &x) {
+  return x.device()->transpose(x);
+}
+
+Tensor dot(const Tensor &a, const Tensor &b) {
+  return a.device()->dot(a, b);
+}
+
+Tensor exp(const Tensor &x) {
+  return x.device()->exp(x);
+}
+
+Tensor tanh(const Tensor &x) {
+  return x.device()->tanh(x);
+}
+
+Tensor sigmoid(const Tensor &x) {
+  return x.device()->sigmoid(x);
+}
+
+Tensor step(const Tensor &x) {
+  return x.device()->step(x);
+}
+
+Tensor relu(const Tensor &x) {
+  return x.device()->relu(x);
+}
+
+Tensor batch_sum(const Tensor &x) {
+  return x.device()->batch_sum(x);
+}
+
+}  // namespace tensor_ops
+}  // namespace primitiv
--- primitiv/tensor_ops.h
+++ primitiv/tensor_ops.h
@@ -7,41 +7,35 @@
 
 namespace primitiv {
 
-inline Tensor operator+(const Tensor &x) { return x.device()->duplicate(x); }
-inline Tensor operator-(const Tensor &x) { return x.device()->negate(x); }
-inline Tensor operator+(const Tensor &x, float k) { return x.device()->add(x, k); }
-inline Tensor operator+(float k, const Tensor &x) { return x.device()->add(x, k); }
-inline Tensor operator+(const Tensor &a, const Tensor &b) { return a.device()->add(a, b); }
-inline Tensor operator-(const Tensor &x, float k) { return x.device()->subtract(x, k); }
-inline Tensor operator-(float k, const Tensor &x) { return x.device()->subtract(k, x); }
-inline Tensor operator-(const Tensor &a, const Tensor &b) { return a.device()->subtract(a, b); }
-inline Tensor operator*(const Tensor &x, float k) { return x.device()->multiply(x, k); }
-inline Tensor operator*(float k, const Tensor &x) { return x.device()->multiply(x, k); }
-inline Tensor operator*(const Tensor &a, const Tensor &b) { return a.device()->multiply(a, b); }
-inline Tensor operator/(const Tensor &x, float k) { return x.device()->divide(x, k); }
-inline Tensor operator/(float k, const Tensor &x) { return x.device()->divide(k, x); }
-inline Tensor operator/(const Tensor &a, const Tensor &b) { return a.device()->divide(a, b); }
+Tensor operator+(const Tensor &x);
+Tensor operator-(const Tensor &x);
+Tensor operator+(const Tensor &x, float k);
+Tensor operator+(float k, const Tensor &x);
+Tensor operator+(const Tensor &a, const Tensor &b);
+Tensor operator-(const Tensor &x, float k);
+Tensor operator-(float k, const Tensor &x);
+Tensor operator-(const Tensor &a, const Tensor &b);
+Tensor operator*(const Tensor &x, float k);
+Tensor operator*(float k, const Tensor &x);
+Tensor operator*(const Tensor &a, const Tensor &b);
+Tensor operator/(const Tensor &x, float k);
+Tensor operator/(float k, const Tensor &x);
+Tensor operator/(const Tensor &a, const Tensor &b);
 
 namespace tensor_ops {
 
-inline Tensor slice(
-    const Tensor &x, unsigned dim, unsigned lower, unsigned upper) {
-  return x.device()->slice(x, dim, lower, upper);
-}
-inline Tensor concat(const std::vector<const Tensor *> &xs, unsigned dim) {
-  if (xs.empty()) THROW_ERROR("No tensors to be concatenated.");
-  return xs[0]->device()->concat(xs, dim);
-}
+Tensor slice(const Tensor &x, unsigned dim, unsigned lower, unsigned upper);
+Tensor concat(const std::vector<const Tensor *> &xs, unsigned dim);
 
-inline Tensor transpose(const Tensor &x) { return x.device()->transpose(x); }
-inline Tensor dot(const Tensor &a, const Tensor &b) { return a.device()->dot(a, b); }
-inline Tensor exp(const Tensor &x) { return x.device()->exp(x); }
-inline Tensor tanh(const Tensor &x) { return x.device()->tanh(x); }
-inline Tensor sigmoid(const Tensor &x) { return x.device()->sigmoid(x); }
-inline Tensor step(const Tensor &x) { return x.device()->step(x); }
-inline Tensor relu(const Tensor &x) { return x.device()->relu(x); }
+Tensor transpose(const Tensor &x);
+Tensor dot(const Tensor &a, const Tensor &b);
+Tensor exp(const Tensor &x);
+Tensor tanh(const Tensor &x);
+Tensor sigmoid(const Tensor &x);
+Tensor step(const Tensor &x);
+Tensor relu(const Tensor &x);
 
-inline Tensor batch_sum(const Tensor &x) { return x.device()->batch_sum(x); }
+Tensor batch_sum(const Tensor &x);
 
 }  // namespace tensor_ops
 }  // namespace primitiv
