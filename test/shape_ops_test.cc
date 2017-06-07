0dca84d76bb76f373a77e3e4dc746b6bcea373e2
odashi <yus.takara@gmail.com>
2017-06-08 00:56:32 +0900

    add throw test of shape_ops::slice.

--- test/shape_ops_test.cc
+++ test/shape_ops_test.cc
@@ -33,5 +33,22 @@ TEST_F(ShapeOpsTest, CheckSlice) {
   }
 }
 
+TEST_F(ShapeOpsTest, CheckInvalidSlice) {
+  struct TestCase {
+    unsigned dim, lower, upper;
+    Shape input;
+  };
+  const vector<TestCase> test_cases {
+    {0, 0, 0, {}}, {0, 1, 0, {}}, {0, 0, 2, {}},
+    {1, 0, 0, {}}, {1, 1, 0, {}}, {1, 1, 2, {}},
+    {0, 0, 0, {2}}, {0, 0, 3, {2}}, {0, 2, 1, {2}}, {1, 0, 2, {2}},
+    {0, 3, 4, {3, 4}}, {0, 3, 2, {3, 4}}, {1, 0, 5, {3, 4}}, {1, 3, 2, {3, 4}},
+    {2, 0, 2, {3, 4}},
+  };
+  for (const TestCase &tc : test_cases) {
+    EXPECT_THROW(slice(tc.input, tc.dim, tc.lower, tc.upper), Error);
+  }
+}
+
 }  // namespace shape_ops
 }  // namespace primitiv
