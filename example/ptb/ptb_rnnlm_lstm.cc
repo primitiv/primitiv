9db600ddab97561e5b6f3b917ce0f6036b6dc1a6
odashi <yus.takara@gmail.com>
2017-07-07 05:00:35 +0900

    add ptb_rnnlm_lstm example.

---
+++ example/ptb/ptb_rnnlm_lstm.cc
@@ -0,0 +1,304 @@
+// Sample code to train the LSTM-based RNNLM using preprocessed Penn Treebank
+// dataset:
+//   http://www.fit.vutbr.cz/~imikolov/rnnlm/simple-examples.tgz
+//
+// Usage:
+//   Run 'download_ptb.sh' in the same directory before using this code.
+//   g++ -std=c++11 \
+//       -I/path/to/primitiv/include \
+//       -I/path/to/cuda/include \
+//       -L/path/to/primitiv/lib \
+//       ptb_rnnlm_lstm.sh \
+//       -lprimitiv
+
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include <primitiv/primitiv.h>
+#include <primitiv/primitiv_cuda.h>
+
+using primitiv::initializers::Constant;
+using primitiv::initializers::XavierUniform;
+using primitiv::trainers::SGD;
+namespace F = primitiv::node_ops;
+using namespace primitiv;
+using namespace std;
+
+namespace {
+
+static const unsigned NUM_EMBED_UNITS = 256;
+static const unsigned NUM_HIDDEN_UNITS = 256;
+static const unsigned BATCH_SIZE = 64;
+static const unsigned MAX_EPOCH = 100;
+
+// Gathers the set of words from space-separated corpus.
+unordered_map<string, unsigned> make_vocab(const string &filename) {
+  ifstream ifs(filename);
+  if (!ifs.is_open()) {
+    cerr << "File could not be opened: " << filename << endl;
+    exit(1);
+  }
+  unordered_map<string, unsigned> vocab;
+  string line, word;
+  unsigned lno = 0;
+  while (getline(ifs, line)) {
+    line = "<s>" + line + "<s>";
+    stringstream ss(line);
+    while (getline(ss, word, ' ')) {
+      if (vocab.find(word) == vocab.end()) {
+        const unsigned id = vocab.size();
+        vocab.emplace(make_pair(word, id));
+      }
+    }
+  }
+  return vocab;
+}
+
+// Generates word ID list using corpus and vocab.
+vector<vector<unsigned>> load_corpus(
+    const string &filename, const unordered_map<string, unsigned> &vocab) {
+  ifstream ifs(filename);
+  if (!ifs.is_open()) {
+    cerr << "File could not be opened: " << filename << endl;
+    exit(1);
+  }
+  vector<vector<unsigned>> corpus;
+  string line, word;
+  while (getline(ifs, line)) {
+    line = "<s>" + line + "<s>";
+    stringstream ss (line);
+    vector<unsigned> sentence;
+    while (getline(ss, word, ' ')) {
+      sentence.emplace_back(vocab.at(word));
+    }
+    corpus.emplace_back(move(sentence));
+  }
+  return corpus;
+}
+
+// Counts output labels in the corpus.
+unsigned count_labels(const vector<vector<unsigned>> &corpus) {
+  unsigned ret = 0;
+  for (const auto &sent :corpus) ret += sent.size() - 1;
+  return ret;
+}
+
+// Extracts a minibatch from loaded corpus
+vector<vector<unsigned>> make_batch(
+    const vector<vector<unsigned>> &corpus,
+    const vector<unsigned> &sent_ids,
+    unsigned eos_id) {
+  const unsigned batch_size = sent_ids.size();
+  unsigned max_len = 0;
+  for (const unsigned sid : sent_ids) {
+    max_len = std::max<unsigned>(max_len, corpus[sid].size());
+  }
+  vector<vector<unsigned>> batch(max_len, vector<unsigned>(batch_size, eos_id));
+  for (unsigned i = 0; i < batch_size; ++i) {
+    const auto &sent = corpus[sent_ids[i]];
+    for (unsigned j = 0; j < sent.size(); ++j) {
+      batch[j][i] = sent[j];
+    }
+  }
+  return batch;
+}
+
+// Hand-written LSTM with input/forget/output gates and no peepholes.
+// Formulation:
+//   i = sigmoid(W_xi . x[t] + W_hi . h[t-1] + b_i)
+//   f = sigmoid(W_xf . x[t] + W_hf . h[t-1] + b_f)
+//   o = sigmoid(W_xo . x[t] + W_ho . h[t-1] + b_o)
+//   j = tanh   (W_xj . x[t] + W_hj . h[t-1] + b_j)
+//   c[t] = i * j + f * c[t-1]
+//   h[t] = o * tanh(c[t])
+class LSTM {
+  public:
+    LSTM(unsigned in_size, unsigned out_size, Device &dev, Trainer &trainer)
+      : out_size_(out_size)
+      , dev_(dev)
+      , pwxh_("wxh", {4 * out_size, in_size}, XavierUniform(), &dev)
+      , pwhh_("whh", {4 * out_size, out_size}, XavierUniform(), &dev)
+      , pbh_("bh", {4 * out_size}, Constant(0), &dev) {
+        trainer.add_parameter(&pwxh_);
+        trainer.add_parameter(&pwhh_);
+        trainer.add_parameter(&pbh_);
+      }
+
+    // Initializes internal values.
+    void init(Graph &g) {
+      wxh_ = F::input(&pwxh_, &g);
+      whh_ = F::input(&pwhh_, &g);
+      bh_ = F::input(&pbh_, &g);
+      h_ = c_ = F::zeros({out_size_}, &dev_, &g);
+    }
+
+    // Forward one step.
+    Node forward(const Node &x) {
+      const Node u = F::matmul(wxh_, x) + F::matmul(whh_, h_) + bh_;
+      const Node i = F::sigmoid(F::slice(u, 0, 0, out_size_));
+      const Node f = F::sigmoid(F::slice(u, 0, out_size_, 2 * out_size_));
+      const Node o = F::sigmoid(F::slice(u, 0, 2 * out_size_, 3 * out_size_));
+      const Node j = F::tanh(F::slice(u, 0, 3 * out_size_, 4 * out_size_));
+      c_ = i * j + f * c_;
+      h_ = o * F::tanh(c_);
+      return h_;
+    }
+
+  private:
+    unsigned out_size_;
+    Device &dev_;
+    Parameter pwxh_, pwhh_, pbh_;
+    Node wxh_, whh_, bh_, h_, c_;
+};
+
+// Language model using above LSTM.
+class RNNLM {
+public:
+  RNNLM(unsigned vocab_size, unsigned eos_id, Device &dev, Trainer &trainer)
+    : dev_(dev)
+    , eos_id_(eos_id)
+    , plookup_("lookup", {NUM_EMBED_UNITS, vocab_size}, XavierUniform(), &dev)
+    , pwhy_("why", {vocab_size, NUM_HIDDEN_UNITS}, XavierUniform(), &dev)
+    , pby_("by", {vocab_size}, Constant(0), &dev)
+    , lstm_(NUM_EMBED_UNITS, NUM_HIDDEN_UNITS, dev, trainer) {
+      trainer.add_parameter(&plookup_);
+      trainer.add_parameter(&pwhy_);
+      trainer.add_parameter(&pby_);
+    }
+
+  // Forward function of RNNLM. Input data should be arranged below:
+  // inputs = {
+  //   {sent1_word1, sent2_word1, ..., sentN_word1},  // 1st input (<s>)
+  //   {sent1_word2, sent2_word2, ..., sentN_word2},  // 2nd input/1st output
+  //   ...,
+  //   {sent1_wordM, sent2_wordM, ..., sentN_wordM},  // last output (<s>)
+  // };
+  vector<Node> forward(const vector<vector<unsigned>> &inputs, Graph &g) {
+    const unsigned batch_size = inputs[0].size();
+    Node lookup = F::input(&plookup_, &g);
+    Node why = F::input(&pwhy_, &g);
+    Node by = F::input(&pby_, &g);
+    lstm_.init(g);
+
+    vector<Node> outputs;
+    for (unsigned i = 0; i < inputs.size() - 1; ++i) {
+      Node x = F::pick(lookup, 1, inputs[i]);
+      Node h = lstm_.forward(x);
+      outputs.emplace_back(F::matmul(why, h) + by);
+    }
+    return outputs;
+  }
+
+  // Loss function.
+  Node forward_loss(
+      const vector<Node> &outputs, const vector<vector<unsigned>> &inputs) {
+    vector<Node> losses;
+    for (unsigned i = 0; i < outputs.size(); ++i) {
+      losses.emplace_back(
+          F::softmax_cross_entropy(outputs[i], 0, inputs[i + 1]));
+    }
+    return F::batch::mean(F::sum(losses));
+  }
+
+private:
+  Device &dev_;
+  unsigned eos_id_;
+  Parameter plookup_, pwhy_, pby_;
+  LSTM lstm_;
+};
+
+}  // namespace
+
+int main() {
+  // Loads vocab.
+  const auto vocab = ::make_vocab("data/ptb.train.txt");
+  cout << "#vocab: " << vocab.size() << endl;  // maybe 10000
+  const unsigned eos_id = vocab.at("<s>");
+
+  // Loads all corpus.
+  const auto train_corpus = ::load_corpus("data/ptb.train.txt", vocab);
+  const auto valid_corpus = ::load_corpus("data/ptb.valid.txt", vocab);
+  const unsigned num_train_sents = train_corpus.size();
+  const unsigned num_valid_sents = valid_corpus.size();
+  const unsigned num_train_labels = ::count_labels(train_corpus);
+  const unsigned num_valid_labels = ::count_labels(valid_corpus);
+  cout << "train: " << num_train_sents << " sentences, "
+                    << num_train_labels << " labels" << endl;
+  cout << "valid: " << num_valid_sents << " sentences, "
+                    << num_valid_labels << " labels" << endl;
+
+  // Uses GPU.
+  CUDADevice dev(0);
+
+  // Trainer.
+  SGD trainer(.1);
+
+  // Our LM.
+  ::RNNLM lm(vocab.size(), eos_id, dev, trainer);
+
+  // Batch randomizer.
+  random_device rd;
+  mt19937 rng(rd());
+
+  // Sentence IDs.
+  vector<unsigned> train_ids(num_train_sents);
+  vector<unsigned> valid_ids(num_valid_sents);
+  iota(begin(train_ids), end(train_ids), 0);
+  iota(begin(valid_ids), end(valid_ids), 0);
+
+  // Train/valid loop.
+  for (unsigned epoch = 0; epoch < MAX_EPOCH; ++epoch) {
+    cout << "epoch " << (epoch + 1) << '/' << MAX_EPOCH << ':' << endl;
+    // Shuffles train sentence IDs.
+    shuffle(begin(train_ids), end(train_ids), rng);
+
+    // Training.
+    float train_loss = 0;
+    for (unsigned ofs = 0; ofs < num_train_sents; ofs += BATCH_SIZE) {
+      const vector<unsigned> batch_ids(
+          begin(train_ids) + ofs,
+          begin(train_ids) + std::min<unsigned>(
+            ofs + BATCH_SIZE, num_train_sents));
+      const auto batch = ::make_batch(train_corpus, batch_ids, eos_id);
+      trainer.reset_gradients();
+      {
+        Graph g;
+        const auto outputs = lm.forward(batch, g);
+        const auto loss = lm.forward_loss(outputs, batch);
+        train_loss += g.forward(loss).to_vector()[0] * batch_ids.size();
+        g.backward(loss);
+      }
+      trainer.update(1);
+      cout << ofs << '\r' << flush;
+    }
+    const float train_ppl = std::exp(train_loss / num_train_labels);
+    cout << "  train ppl = " << train_ppl << endl;
+
+    // Validation.
+    float valid_loss = 0;
+    for (unsigned ofs = 0; ofs < num_valid_sents; ofs += BATCH_SIZE) {
+      const vector<unsigned> batch_ids(
+          begin(valid_ids) + ofs,
+          begin(valid_ids) + std::min<unsigned>(
+            ofs + BATCH_SIZE, num_valid_sents));
+      const auto batch = ::make_batch(valid_corpus, batch_ids, eos_id);
+      Graph g;
+      const auto outputs = lm.forward(batch, g);
+      const auto loss = lm.forward_loss(outputs, batch);
+      valid_loss += g.forward(loss).to_vector()[0] * batch_ids.size();
+      cout << ofs << '\r' << flush;
+    }
+    const float valid_ppl = std::exp(valid_loss / num_valid_labels);
+    cout << "  valid ppl = " << valid_ppl << endl;
+  }
+
+  return 0;
+}
