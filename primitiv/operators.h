7910679c238c548286e7b270b13fc4141aeac03f
odashi <yus.takara@gmail.com>
2017-10-03 15:19:57 +0900

    separated type_traits codes.

--- primitiv/operators.h
+++ primitiv/operators.h
@@ -2,47 +2,14 @@
 #define PRIMITIV_OPERATORS_H_
 
 #include <initializer_list>
-#include <type_traits>
 #include <vector>
 
 #include <primitiv/device.h>
 #include <primitiv/error.h>
 #include <primitiv/graph.h>
+#include <primitiv/type_traits.h>
 
 namespace primitiv {
-
-namespace type_traits {
-
-// NOTE(odashi):
-// The template variable `Var` could becomes only either `Tensor` or `Node`.
-
-template<typename Var> struct is_var : std::false_type {};
-template<> struct is_var<Tensor> : std::true_type {};
-template<> struct is_var<Node> : std::true_type {};
-
-template<typename Var>
-using Identity = typename std::enable_if<is_var<Var>::value, Var>::type;
-
-template<typename Container>
-using Reduce = typename std::enable_if<
-  is_var<typename Container::value_type>::value,
-  typename Container::value_type
->::type;
-
-template<typename Container>
-using ReducePtr = typename std::enable_if<
-  std::is_pointer<typename Container::value_type>::value &&
-  is_var<
-    typename std::remove_const<
-      typename std::remove_pointer<typename Container::value_type>::type
-    >::type
-  >::value,
-  typename std::remove_const<
-    typename std::remove_pointer<typename Container::value_type>::type
-  >::type
->::type;
-
-}  // namespace type_traits
 
 class Parameter;
 
