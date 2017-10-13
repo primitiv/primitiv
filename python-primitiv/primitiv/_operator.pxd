e1a7a93b42de786502d3fa00a2a1324f49f84f89
Koichi Akabe <vbkaisetsu@gmail.com>
2017-10-13 20:39:06 +0900

    Remove unused descripters of operator functions

--- python-primitiv/primitiv/_operator.pxd
+++ python-primitiv/primitiv/_operator.pxd
@@ -12,111 +12,62 @@ cdef extern from "operator_template_wrapper.h" namespace "python_primitiv":
 
     CppNode Node_input_vector(const CppShape &shape, const vector[float] &data, CppDevice &dev, CppGraph &g) except +
     CppNode Node_input_vector(const CppShape &shape, const vector[float] &data, CppDevice &dev) except +
-    CppTensor Tensor_input_vector(const CppShape &shape, const vector[float] &data, CppDevice &dev) except +
     CppNode Node_parameter(CppParameter &param, CppGraph &g) except +
     CppNode Node_parameter(CppParameter &param) except +
-    CppTensor Tensor_parameter(CppParameter &param) except +
     CppNode Node_copy(const CppNode &x, CppDevice &dev) except +
-    CppTensor Tensor_copy(const CppTensor &x, CppDevice &dev) except +
     CppNode Node_pick(const CppNode &x, const vector[unsigned] &ids, unsigned dim) except +
-    CppTensor Tensor_pick(const CppTensor &x, const vector[unsigned] &ids, unsigned dim) except +
     CppNode Node_slice(const CppNode &x, unsigned dim, unsigned lower, unsigned upper) except +
-    CppTensor Tensor_slice(const CppTensor &x, unsigned dim, unsigned lower, unsigned upper) except +
     CppNode Node_concat(const vector[CppNode] &xs, unsigned dim) except +
-    CppTensor Tensor_concat(const vector[CppTensor] &xs, unsigned dim) except +
     CppNode Node_reshape(const CppNode &x, const CppShape &new_shape) except +
-    CppTensor Tensor_reshape(const CppTensor &x, const CppShape &new_shape) except +
     CppNode Node_flatten(const CppNode &x) except +
-    CppTensor Tensor_flatten(const CppTensor &x) except +
     CppNode Node_transpose(const CppNode &x) except +
-    CppTensor Tensor_transpose(const CppTensor &x) except +
     CppNode Node_matmul(const CppNode &a, const CppNode &b) except +
-    CppTensor Tensor_matmul(const CppTensor &a, const CppTensor &b) except +
     CppNode Node_sqrt(const CppNode &x) except +
-    CppTensor Tensor_sqrt(const CppTensor &x) except +
     CppNode Node_exp(const CppNode &x) except +
-    CppTensor Tensor_exp(const CppTensor &x) except +
     CppNode Node_log(const CppNode &x) except +
-    CppTensor Tensor_log(const CppTensor &x) except +
     CppNode Node_pow(const CppNode &x, float k) except +
     CppNode Node_pow(float x, const CppNode &k) except +
     CppNode Node_pow(const CppNode &x, const CppNode &k) except +
     CppNode Node_tanh(const CppNode &x) except +
-    CppTensor Tensor_tanh(const CppTensor &x) except +
     CppNode Node_sigmoid(const CppNode &x) except +
-    CppTensor Tensor_sigmoid(const CppTensor &x) except +
     CppNode Node_softplus(const CppNode &x) except +
-    CppTensor Tensor_softplus(const CppTensor &x) except +
     CppNode Node_sin(const CppNode &x) except +
-    CppTensor Tensor_sin(const CppTensor &x) except +
     CppNode Node_cos(const CppNode &x) except +
-    CppTensor Tensor_cos(const CppTensor &x) except +
     CppNode Node_tan(const CppNode &x) except +
-    CppTensor Tensor_tan(const CppTensor &x) except +
     CppNode Node_relu(const CppNode &x) except +
-    CppTensor Tensor_relu(const CppTensor &x) except +
     CppNode Node_lrelu(const CppNode &x) except +
-    CppTensor Tensor_lrelu(const CppTensor &x) except +
     CppNode Node_prelu(const CppNode &x, float a) except +
-    CppTensor Tensor_prelu(const CppTensor &x, float a) except +
     CppNode Node_elu(const CppNode &x, float a) except +
-    CppTensor Tensor_elu(const CppTensor &x, float a) except +
     CppNode Node_selu(const CppNode &x, float a, float s) except +
-    CppTensor Tensor_selu(const CppTensor &x, float a, float s) except +
     CppNode Node_sum(const CppNode &x, unsigned dim) except +
     CppNode Node_sum_container(const vector[CppNode] &xs) except +
-    CppTensor Tensor_sum(const CppTensor &x, unsigned dim) except +
     CppNode Node_mean(const CppNode &x, unsigned dim) except +
     CppNode Node_mean_container(const vector[CppNode] &xs) except +
-    CppTensor Tensor_mean(const CppTensor &x, unsigned dim) except +
     CppNode Node_broadcast(const CppNode &x, unsigned dim, unsigned size) except +
-    CppTensor Tensor_broadcast(const CppTensor &x, unsigned dim, unsigned size) except +
     CppNode Node_logsumexp(const CppNode &x, unsigned dim) except +
-    CppTensor Tensor_logsumexp(const CppTensor &x, unsigned dim) except +
     CppNode Node_log_softmax(const CppNode &x, unsigned dim) except +
-    CppTensor Tensor_log_softmax(const CppTensor &x, unsigned dim) except +
     CppNode Node_softmax(const CppNode &x, unsigned dim) except +
-    CppTensor Tensor_softmax(const CppTensor &x, unsigned dim) except +
     CppNode Node_softmax_cross_entropy(const CppNode &x, const CppNode &t, unsigned dim) except +
-    CppTensor Tensor_softmax_cross_entropy(const CppTensor &x, const CppTensor &t, unsigned dim) except +
     CppNode Node_softmax_cross_entropy(const CppNode &x, const vector[unsigned] &ids, unsigned dim) except +
-    CppTensor Tensor_softmax_cross_entropy(const CppTensor &x, const vector[unsigned] &ids, unsigned dim) except +
-
     CppNode Node_batch_sum(const CppNode &x) except +
-    CppTensor Tensor_batch_sum(const CppTensor &x) except +
     CppNode Node_batch_mean(const CppNode &x) except +
-    CppTensor Tensor_batch_mean(const CppTensor &x) except +
     CppNode Node_batch_normalize(const CppNode &x) except +
-    CppTensor Tensor_batch_normalize(const CppTensor &x) except +
-
     CppNode Node_constant(const CppShape &shape, float k, CppDevice &dev, CppGraph &g) except +
     CppNode Node_zeros(const CppShape &shape, CppDevice &dev, CppGraph &g) except +
     CppNode Node_ones(const CppShape &shape, CppDevice &dev, CppGraph &g) except +
     CppNode Node_identity(unsigned size, CppDevice &dev, CppGraph &g) except +
     CppNode Node_constant(const CppShape &shape, float k, CppDevice &dev) except +
-    CppTensor Tensor_constant(const CppShape &shape, float k, CppDevice &dev) except +
     CppNode Node_zeros(const CppShape &shape, CppDevice &dev) except +
-    CppTensor Tensor_zeros(const CppShape &shape, CppDevice &dev) except +
     CppNode Node_ones(const CppShape &shape, CppDevice &dev) except +
-    CppTensor Tensor_ones(const CppShape &shape, CppDevice &dev) except +
     CppNode Node_identity(unsigned size, CppDevice &dev) except +
-    CppTensor Tensor_identity(unsigned size, CppDevice &dev) except +
-
     CppNode Node_random_bernoulli(const CppShape &shape, float p, CppDevice &dev, CppGraph &g) except +
     CppNode Node_random_bernoulli(const CppShape &shape, float p, CppDevice &dev) except +
-    CppTensor Tensor_random_bernoulli(const CppShape &shape, float p, CppDevice &dev) except +
     CppNode Node_random_uniform(const CppShape &shape, float lower, float upper, CppDevice &dev, CppGraph &g) except +
     CppNode Node_random_uniform(const CppShape &shape, float lower, float upper, CppDevice &dev) except +
-    CppTensor Tensor_random_uniform(const CppShape &shape, float lower, float upper, CppDevice &dev) except +
     CppNode Node_random_normal(const CppShape &shape, float mean, float sd, CppDevice &dev, CppGraph &g) except +
     CppNode Node_random_normal(const CppShape &shape, float mean, float sd, CppDevice &dev) except +
-    CppTensor Tensor_random_normal(const CppShape &shape, float mean, float sd, CppDevice &dev) except +
     CppNode Node_random_log_normal(const CppShape &shape, float mean, float sd, CppDevice &dev, CppGraph &g) except +
     CppNode Node_random_log_normal(const CppShape &shape, float mean, float sd, CppDevice &dev) except +
-    CppTensor Tensor_random_log_normal(const CppShape &shape, float mean, float sd, CppDevice &dev) except +
     CppNode Node_random_gumbel(const CppShape &shape, float mu, float beta, CppDevice &dev, CppGraph &g) except +
     CppNode Node_random_gumbel(const CppShape &shape, float mu, float beta, CppDevice &dev) except +
-    CppTensor Tensor_random_gumbel(const CppShape &shape, float mu, float beta, CppDevice &dev) except +
-
     CppNode Node_dropout(const CppNode &x, float rate, bool enabled) except +
-    CppTensor Tensor_dropout(const CppTensor &x, float rate, bool enabled) except +
--- python-primitiv/primitiv/operator_template_wrapper.h
+++ python-primitiv/primitiv/operator_template_wrapper.h
@@ -11,18 +11,12 @@ namespace python_primitiv {
 
 using namespace primitiv;
 
-// TODO: Tensor operators will be removed.
-
 inline Node Node_input_vector(const Shape &shape, const std::vector<float> &data, Device &dev, Graph &g) {
     return operators::input(shape, data, dev, g);
 }
 
 inline Node Node_input_vector(const Shape &shape, const std::vector<float> &data, Device &dev) {
     return operators::input<Node>(shape, data, dev);
-}
-
-inline Tensor Tensor_input_vector(const Shape &shape, const std::vector<float> &data, Device &dev) {
-    return operators::input<Tensor>(shape, data, dev);
 }
 
 inline Node Node_parameter(Parameter &param, Graph &g) {
@@ -33,96 +27,48 @@ inline Node Node_parameter(Parameter &param) {
     return operators::parameter<Node>(param);
 }
 
-inline Tensor Tensor_parameter(Parameter &param) {
-    return operators::parameter<Tensor>(param);
-}
-
 inline Node Node_copy(const Node &x, Device &dev) {
     return operators::copy<Node>(x, dev);
-}
-
-inline Tensor Tensor_copy(const Tensor &x, Device &dev) {
-    return operators::copy<Tensor>(x, dev);
 }
 
 inline Node Node_pick(const Node &x, const std::vector<unsigned> &ids, unsigned dim) {
     return operators::pick<Node>(x, ids, dim);
 }
 
-inline Tensor Tensor_pick(const Tensor &x, const std::vector<unsigned> &ids, unsigned dim) {
-    return operators::pick<Tensor>(x, ids, dim);
-}
-
 inline Node Node_slice(const Node &x, unsigned dim, unsigned lower, unsigned upper) {
     return operators::slice<Node>(x, dim, lower, upper);
-}
-
-inline Tensor Tensor_slice(const Tensor &x, unsigned dim, unsigned lower, unsigned upper) {
-    return operators::slice<Tensor>(x, dim, lower, upper);
 }
 
 inline Node Node_concat(const std::vector<Node> &xs, unsigned dim) {
     return operators::concat<Node>(xs, dim);
 }
 
-inline Tensor Tensor_concat(const std::vector<Tensor> &xs, unsigned dim) {
-    return operators::concat<Tensor>(xs, dim);
-}
-
 inline Node Node_reshape(const Node &x, const Shape &new_shape) {
     return operators::reshape<Node>(x, new_shape);
-}
-
-inline Tensor Tensor_reshape(const Tensor &x, const Shape &new_shape) {
-    return operators::reshape<Tensor>(x, new_shape);
 }
 
 inline Node Node_flatten(const Node &x) {
     return operators::flatten<Node>(x);
 }
 
-inline Tensor Tensor_flatten(const Tensor &x) {
-    return operators::flatten<Tensor>(x);
-}
-
 inline Node Node_transpose(const Node &x) {
     return operators::transpose<Node>(x);
-}
-
-inline Tensor Tensor_transpose(const Tensor &x) {
-    return operators::transpose<Tensor>(x);
 }
 
 inline Node Node_matmul(const Node &a, const Node &b) {
     return operators::matmul<Node>(a, b);
 }
 
-inline Tensor Tensor_matmul(const Tensor &a, const Tensor &b) {
-    return operators::matmul<Tensor>(a, b);
-}
-
 inline Node Node_sqrt(const Node &x) {
     return operators::sqrt<Node>(x);
-}
-
-inline Tensor Tensor_sqrt(const Tensor &x) {
-    return operators::sqrt<Tensor>(x);
 }
 
 inline Node Node_exp(const Node &x) {
     return operators::exp<Node>(x);
 }
 
-inline Tensor Tensor_exp(const Tensor &x) {
-    return operators::exp<Tensor>(x);
-}
-
 inline Node Node_log(const Node &x) {
     return operators::log<Node>(x);
-}
-
-inline Tensor Tensor_log(const Tensor &x) {
-    return operators::log<Tensor>(x);
 }
 
 inline Node Node_pow(const Node &x, float k) {
@@ -141,88 +87,44 @@ inline Node Node_tanh(const Node &x) {
     return operators::tanh<Node>(x);
 }
 
-inline Tensor Tensor_tanh(const Tensor &x) {
-    return operators::tanh<Tensor>(x);
-}
-
 inline Node Node_sigmoid(const Node &x) {
     return operators::sigmoid<Node>(x);
-}
-
-inline Tensor Tensor_sigmoid(const Tensor &x) {
-    return operators::sigmoid<Tensor>(x);
 }
 
 inline Node Node_softplus(const Node &x) {
     return operators::softplus<Node>(x);
 }
 
-inline Tensor Tensor_softplus(const Tensor &x) {
-    return operators::softplus<Tensor>(x);
-}
-
 inline Node Node_sin(const Node &x) {
     return operators::sin<Node>(x);
-}
-
-inline Tensor Tensor_sin(const Tensor &x) {
-    return operators::sin<Tensor>(x);
 }
 
 inline Node Node_cos(const Node &x) {
     return operators::cos<Node>(x);
 }
 
-inline Tensor Tensor_cos(const Tensor &x) {
-    return operators::cos<Tensor>(x);
-}
-
 inline Node Node_tan(const Node &x) {
     return operators::tan<Node>(x);
-}
-
-inline Tensor Tensor_tan(const Tensor &x) {
-    return operators::tan<Tensor>(x);
 }
 
 inline Node Node_relu(const Node &x) {
     return operators::relu<Node>(x);
 }
 
-inline Tensor Tensor_relu(const Tensor &x) {
-    return operators::relu<Tensor>(x);
-}
-
 inline Node Node_lrelu(const Node &x) {
     return operators::lrelu<Node>(x);
-}
-
-inline Tensor Tensor_lrelu(const Tensor &x) {
-    return operators::lrelu<Tensor>(x);
 }
 
 inline Node Node_prelu(const Node &x, float a) {
     return operators::prelu<Node>(x, a);
 }
 
-inline Tensor Tensor_prelu(const Tensor &x, float a) {
-    return operators::prelu<Tensor>(x, a);
-}
-
 inline Node Node_elu(const Node &x, float a) {
     return operators::elu<Node>(x, a);
 }
 
-inline Tensor Tensor_elu(const Tensor &x, float a) {
-    return operators::elu<Tensor>(x, a);
-}
-
 inline Node Node_selu(const Node &x, float a, float s) {
     return operators::selu<Node>(x, a, s);
-}
-
-inline Tensor Tensor_selu(const Tensor &x, float a, float s) {
-    return operators::selu<Tensor>(x, a, s);
 }
 
 inline Node Node_sum(const Node &x, unsigned dim) {
@@ -233,10 +135,6 @@ inline Node Node_sum_container(const vector<Node> &xs) {
     return operators::sum<vector<Node>>(xs);
 }
 
-inline Tensor Tensor_sum(const Tensor &x, unsigned dim) {
-    return operators::sum<Tensor>(x, dim);
-}
-
 inline Node Node_mean(const Node &x, unsigned dim) {
     return operators::mean<Node>(x, dim);
 }
@@ -245,80 +143,40 @@ inline Node Node_mean_container(const vector<Node> &xs) {
     return operators::mean<vector<Node>>(xs);
 }
 
-inline Tensor Tensor_mean(const Tensor &x, unsigned dim) {
-    return operators::mean<Tensor>(x, dim);
-}
-
 inline Node Node_broadcast(const Node &x, unsigned dim, unsigned size) {
     return operators::broadcast<Node>(x, dim, size);
-}
-
-inline Tensor Tensor_broadcast(const Tensor &x, unsigned dim, unsigned size) {
-    return operators::broadcast<Tensor>(x, dim, size);
 }
 
 inline Node Node_logsumexp(const Node &x, unsigned dim) {
     return operators::logsumexp<Node>(x, dim);
 }
 
-inline Tensor Tensor_logsumexp(const Tensor &x, unsigned dim) {
-    return operators::logsumexp<Tensor>(x, dim);
-}
-
 inline Node Node_log_softmax(const Node &x, unsigned dim) {
     return operators::log_softmax<Node>(x, dim);
-}
-
-inline Tensor Tensor_log_softmax(const Tensor &x, unsigned dim) {
-    return operators::log_softmax<Tensor>(x, dim);
 }
 
 inline Node Node_softmax(const Node &x, unsigned dim) {
     return operators::softmax<Node>(x, dim);
 }
 
-inline Tensor Tensor_softmax(const Tensor &x, unsigned dim) {
-    return operators::softmax<Tensor>(x, dim);
-}
-
 inline Node Node_softmax_cross_entropy(const Node &x, const Node &t, unsigned dim) {
     return operators::softmax_cross_entropy<Node>(x, t, dim);
-}
-
-inline Tensor Tensor_softmax_cross_entropy(const Tensor &x, const Tensor &t, unsigned dim) {
-    return operators::softmax_cross_entropy<Tensor>(x, t, dim);
 }
 
 inline Node Node_softmax_cross_entropy(const Node &x, const std::vector<unsigned> &ids, unsigned dim) {
     return operators::softmax_cross_entropy<Node>(x, ids, dim);
 }
 
-inline Tensor Tensor_softmax_cross_entropy(const Tensor &x, const std::vector<unsigned> &ids, unsigned dim) {
-    return operators::softmax_cross_entropy<Tensor>(x, ids, dim);
-}
-
 inline Node Node_batch_sum(const Node &x) {
     return operators::batch::sum<Node>(x);
-}
-
-inline Tensor Tensor_batch_sum(const Tensor &x) {
-    return operators::batch::sum<Tensor>(x);
 }
 
 inline Node Node_batch_mean(const Node &x) {
     return operators::batch::mean<Node>(x);
 }
 
-inline Tensor Tensor_batch_mean(const Tensor &x) {
-    return operators::batch::mean<Tensor>(x);
-}
-
 inline Node Node_batch_normalize(const Node &x) {
     return operators::batch::normalize<Node>(x);
-}
-
-inline Tensor Tensor_batch_normalize(const Tensor &x) {
-    return operators::batch::normalize<Tensor>(x);
 }
 
 inline Node Node_constant(const Shape &shape, float k, Device &dev, Graph &g) {
@@ -341,32 +199,16 @@ inline Node Node_constant(const Shape &shape, float k, Device &dev) {
     return operators::constant<Node>(shape, k, dev);
 }
 
-inline Tensor Tensor_constant(const Shape &shape, float k, Device &dev) {
-    return operators::constant<Tensor>(shape, k, dev);
-}
-
 inline Node Node_zeros(const Shape &shape, Device &dev) {
     return operators::zeros<Node>(shape, dev);
-}
-
-inline Tensor Tensor_zeros(const Shape &shape, Device &dev) {
-    return operators::zeros<Tensor>(shape, dev);
 }
 
 inline Node Node_ones(const Shape &shape, Device &dev) {
     return operators::ones<Node>(shape, dev);
 }
 
-inline Tensor Tensor_ones(const Shape &shape, Device &dev) {
-    return operators::ones<Tensor>(shape, dev);
-}
-
 inline Node Node_identity(unsigned size, Device &dev) {
     return operators::identity<Node>(size, dev);
-}
-
-inline Tensor Tensor_identity(unsigned size, Device &dev) {
-    return operators::identity<Tensor>(size, dev);
 }
 
 inline Node Node_random_bernoulli(const Shape &shape, float p, Device &dev, Graph &g) {
@@ -377,20 +219,12 @@ inline Node Node_random_bernoulli(const Shape &shape, float p, Device &dev) {
     return operators::random::bernoulli<Node>(shape, p, dev);
 }
 
-inline Tensor Tensor_random_bernoulli(const Shape &shape, float p, Device &dev) {
-    return operators::random::bernoulli<Tensor>(shape, p, dev);
-}
-
 inline Node Node_random_uniform(const Shape &shape, float lower, float upper, Device &dev, Graph &g) {
     return operators::random::uniform(shape, lower, upper, dev, g);
 }
 
 inline Node Node_random_uniform(const Shape &shape, float lower, float upper, Device &dev) {
     return operators::random::uniform<Node>(shape, lower, upper, dev);
-}
-
-inline Tensor Tensor_random_uniform(const Shape &shape, float lower, float upper, Device &dev) {
-    return operators::random::uniform<Tensor>(shape, lower, upper, dev);
 }
 
 inline Node Node_random_normal(const Shape &shape, float mean, float sd, Device &dev, Graph &g) {
@@ -401,20 +235,12 @@ inline Node Node_random_normal(const Shape &shape, float mean, float sd, Device 
     return operators::random::normal<Node>(shape, mean, sd, dev);
 }
 
-inline Tensor Tensor_random_normal(const Shape &shape, float mean, float sd, Device &dev) {
-    return operators::random::normal<Tensor>(shape, mean, sd, dev);
-}
-
 inline Node Node_random_log_normal(const Shape &shape, float mean, float sd, Device &dev, Graph &g) {
     return operators::random::log_normal(shape, mean, sd, dev, g);
 }
 
 inline Node Node_random_log_normal(const Shape &shape, float mean, float sd, Device &dev) {
     return operators::random::log_normal<Node>(shape, mean, sd, dev);
-}
-
-inline Tensor Tensor_random_log_normal(const Shape &shape, float mean, float sd, Device &dev) {
-    return operators::random::log_normal<Tensor>(shape, mean, sd, dev);
 }
 
 inline Node Node_random_gumbel(const Shape &shape, float mu, float beta, Device &dev, Graph &g) {
@@ -425,16 +251,8 @@ inline Node Node_random_gumbel(const Shape &shape, float mu, float beta, Device 
     return operators::random::gumbel<Node>(shape, mu, beta, dev);
 }
 
-inline Tensor Tensor_random_gumbel(const Shape &shape, float mu, float beta, Device &dev) {
-    return operators::random::gumbel<Tensor>(shape, mu, beta, dev);
-}
-
 inline Node Node_dropout(const Node &x, float rate, bool enabled) {
     return operators::dropout<Node>(x, rate, enabled);
-}
-
-inline Tensor Tensor_dropout(const Tensor &x, float rate, bool enabled) {
-    return operators::dropout<Tensor>(x, rate, enabled);
 }
 
 }
