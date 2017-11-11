dbd05cf9c5604ab99022acfaeb87b9c2c7a83abe
Koichi Akabe <vbkaisetsu@gmail.com>
2017-11-11 11:58:49 +0900

    Update tests

--- python-primitiv/tests/model.py
+++ python-primitiv/tests/model.py
@@ -6,6 +6,11 @@ from primitiv._model import _ModelSubModel
 import unittest
 
 
+class TestModel(Model):
+    def __init__(self):
+        super().__init__()
+
+
 class ModelTest(unittest.TestCase):
 
     @classmethod
@@ -24,40 +29,68 @@ class ModelTest(unittest.TestCase):
          pass
 
     def test_model_parameter(self):
-        model = Model()
+        model = TestModel()
         param = Parameter()
         model.add_parameter("p", param)
         self.assertIs(model.params["p"], param)
 
     def test_model_parameter_deep(self):
-        model1 = Model()
-        model2 = Model()
+        model1 = TestModel()
+        model2 = TestModel()
         model1.add_submodel("m2", model2)
-        model3 = Model()
+        model3 = TestModel()
         model2.add_submodel("m3", model3)
         param = Parameter()
         model3.add_parameter("p", param)
         self.assertIs(model1.params["m2", "m3", "p"], param)
 
     def test_model_submodel(self):
-        model1 = Model()
-        model2 = Model()
+        model1 = TestModel()
+        model2 = TestModel()
         model1.add_submodel("m", model2)
         self.assertIs(model1.submodels["m"], model2)
 
     def test_model_submodel_deep(self):
-        model1 = Model()
-        model2 = Model()
+        model1 = TestModel()
+        model2 = TestModel()
         model1.add_submodel("m2", model2)
-        model3 = Model()
+        model3 = TestModel()
         model2.add_submodel("m3", model3)
-        model4 = Model()
+        model4 = TestModel()
         model3.add_submodel("m4", model4)
         self.assertIs(model1.submodels["m2", "m3", "m4"], model4)
 
+    def test_model_auto_add_attributes(self):
+        model1 = TestModel()
+        model1.param1 = Parameter()
+        model1.add_parameter("p1", model1.param1)
+        model1.model2 = TestModel()
+        model1.add_submodel("m2", model1.model2)
+        model1.param2 = Parameter()
+        model1.model3 = TestModel()
+        model1.auto_add_attributes()
+        self.assertIs(model1.params["p1"], model1.param1)
+        self.assertIs(model1.submodels["m2"], model1.model2)
+        self.assertIs(model1.params["param2"], model1.param2)
+        self.assertIs(model1.submodels["model3"], model1.model3)
+        with self.assertRaises(RuntimeError):
+            p2 = model1.params["param1"]
+        with self.assertRaises(RuntimeError):
+            m3 = model1.submodels["model2"]
+
+    def test_model_auto_add_attributes_duplicate(self):
+        model1 = TestModel()
+        model1.param1 = Parameter()
+        model1.param2 = Parameter()
+        model1.param3 = model1.param2
+        with self.assertRaises(ValueError):
+            model1.auto_add_attributes()
+        with self.assertRaises(RuntimeError):
+            p1 = model1.params["param1"]
+
     def test_model_invalid_operation(self):
-        model1 = Model()
-        model2 = Model()
+        model1 = TestModel()
+        model2 = TestModel()
         model1.add_submodel("m", model2)
         param = Parameter()
         model1.add_parameter("p", param)
