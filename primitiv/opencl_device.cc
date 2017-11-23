3512365014697f4019659e910d5c82bee0a41cd8
Koichi Akabe <vbkaisetsu@gmail.com>
2017-11-23 09:47:15 +0900

    Replace deprecated flag __CL_ENABLE_EXCEPTIONS with CL_HPP_ENABLE_EXCEPTIONS

--- primitiv/opencl_device.cc
+++ primitiv/opencl_device.cc
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <random>
 
-#define __CL_ENABLE_EXCEPTIONS
+#define CL_HPP_ENABLE_EXCEPTIONS
 #define CL_HPP_MINIMUM_OPENCL_VERSION 120
 #define CL_HPP_TARGET_OPENCL_VERSION 120
 #define CL_HPP_CL_1_2_DEFAULT_BUILD
