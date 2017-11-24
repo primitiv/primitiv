a1e580fa625cf53073b176f63b606dc1d2c926f5
Koichi Akabe <vbkaisetsu@gmail.com>
2017-11-24 22:35:23 +0900

    Fix variable name

--- cmake/generate_char_array.cmake
+++ cmake/generate_char_array.cmake
@@ -1,3 +1,3 @@
 file(READ ${CMAKE_ARGV3} hexdata HEX)
-string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1, " PRIMITIV_OPENCL_KERNEL_CHAR_ARRAY ${hexdata})
-file(WRITE ${CMAKE_ARGV4} ${PRIMITIV_OPENCL_KERNEL_CHAR_ARRAY})
+string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1, " hex_split ${hexdata})
+file(WRITE ${CMAKE_ARGV4} ${hex_split})
