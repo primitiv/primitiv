a1ea2fbebe58f9dc7f5246ca34be274fb1aa9e38
odashi <yus.takara@gmail.com>
2017-12-04 20:45:40 +0900

    Add RecursiveSpinlock.

--- test/spinlock_test.cc
+++ test/spinlock_test.cc
@@ -35,4 +35,38 @@ TEST_F(SpinlockTest, CheckBlock) {
   EXPECT_EQ(4000000, y);
 }
 
+class RecursiveSpinlockTest : public testing::Test {};
+
+TEST_F(RecursiveSpinlockTest, CheckBlock) {
+  RecursiveSpinlock sl;
+  int x = 0;
+
+  auto proc = [&] {
+    for (int i = 0; i < 1000000; ++i) {
+      {
+        std::lock_guard<RecursiveSpinlock> lock1(sl);
+        {
+          std::lock_guard<RecursiveSpinlock> lock2(sl);
+          {
+            std::lock_guard<RecursiveSpinlock> lock3(sl);
+            ++x;
+          }
+        }
+      }
+    }
+  };
+
+  std::thread th1(proc);
+  std::thread th2(proc);
+  std::thread th3(proc);
+  std::thread th4(proc);
+
+  th1.join();
+  th2.join();
+  th3.join();
+  th4.join();
+
+  EXPECT_EQ(4000000, x);
+}
+
 }  // namespace primitiv
