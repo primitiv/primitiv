7212777d29b8600b1252b90f3cfba3b975210353
odashi <yus.takara@gmail.com>
2017-12-07 16:22:19 +0900

    Change Tensor::shape() to return a Shape object.

--- primitiv/device.cc
+++ primitiv/device.cc
@@ -157,11 +157,14 @@ Tensor Device::slice_fw(
 
 Tensor Device::concat_fw(const vector<const Tensor *> &xs, std::uint32_t dim) {
   if (xs.empty()) THROW_ERROR("No tensors to concat.");
-  vector<const Shape *> shapes(xs.size());
+  vector<Shape> shapes;
+  shapes.reserve(xs.size());
+
   for (std::uint32_t i = 0; i < xs.size(); ++i) {
     CHECK_DEVICE(*xs[i]);
-    shapes[i] = &xs[i]->shape();
+    shapes.emplace_back(xs[i]->shape());
   }
+
   Tensor y = new_raw_tensor(shape_ops::concat(shapes, dim));
   concat_fw_impl(xs, dim, y);
   return y;
