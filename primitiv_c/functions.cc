8fe7cb76162086c133c275a85bee00800ea97bb4
Hiroki Teranishi <teranishihiroki@gmail.com>
2017-12-08 14:23:03 +0900

    Rename `add`, `sub`, `mul`, and `div` operator functions.

--- primitiv_c/functions.cc
+++ primitiv_c/functions.cc
@@ -10,122 +10,122 @@ using primitiv::Tensor;
 
 extern "C" {
 
-primitiv_Node *primitiv_func_node_add_const(const primitiv_Node *x, float k) {
+primitiv_Node *primitiv_node_func_node_add_const(const primitiv_Node *x, float k) {
   Node y = *to_cc(x) + k;
   return to_c_from_value(y);
 }
 
-primitiv_Node *primitiv_func_const_add_node(float k, const primitiv_Node *x) {
+primitiv_Node *primitiv_node_func_const_add_node(float k, const primitiv_Node *x) {
   Node y = k + *to_cc(x);
   return to_c_from_value(y);
 }
 
-primitiv_Node *primitiv_func_node_add_node(const primitiv_Node *a, const primitiv_Node *b) {
+primitiv_Node *primitiv_node_func_node_add_node(const primitiv_Node *a, const primitiv_Node *b) {
   Node y = *to_cc(a) + *to_cc(b);
   return to_c_from_value(y);
 }
 
-primitiv_Node *primitiv_func_node_sub_const(const primitiv_Node *x, float k) {
+primitiv_Node *primitiv_node_func_node_sub_const(const primitiv_Node *x, float k) {
   Node y = *to_cc(x) - k;
   return to_c_from_value(y);
 }
 
-primitiv_Node *primitiv_func_const_sub_node(float k, const primitiv_Node *x) {
+primitiv_Node *primitiv_node_func_const_sub_node(float k, const primitiv_Node *x) {
   Node y = k - *to_cc(x);
   return to_c_from_value(y);
 }
 
-primitiv_Node *primitiv_func_node_sub_node(const primitiv_Node *a, const primitiv_Node *b) {
+primitiv_Node *primitiv_node_func_node_sub_node(const primitiv_Node *a, const primitiv_Node *b) {
   Node y = *to_cc(a) - *to_cc(b);
   return to_c_from_value(y);
 }
 
-primitiv_Node *primitiv_func_node_mul_const(const primitiv_Node *x, float k) {
+primitiv_Node *primitiv_node_func_node_mul_const(const primitiv_Node *x, float k) {
   Node y = *to_cc(x) * k;
   return to_c_from_value(y);
 }
 
-primitiv_Node *primitiv_func_const_mul_node(float k, const primitiv_Node *x) {
+primitiv_Node *primitiv_node_func_const_mul_node(float k, const primitiv_Node *x) {
   Node y = k * *to_cc(x);
   return to_c_from_value(y);
 }
 
-primitiv_Node *primitiv_func_node_mul_node(const primitiv_Node *a, const primitiv_Node *b) {
+primitiv_Node *primitiv_node_func_node_mul_node(const primitiv_Node *a, const primitiv_Node *b) {
   Node y = *to_cc(a) * *to_cc(b);
   return to_c_from_value(y);
 }
 
-primitiv_Node *primitiv_func_node_div_const(const primitiv_Node *x, float k) {
+primitiv_Node *primitiv_node_func_node_div_const(const primitiv_Node *x, float k) {
   Node y = *to_cc(x) / k;
   return to_c_from_value(y);
 }
 
-primitiv_Node *primitiv_func_const_div_node(float k, const primitiv_Node *x) {
+primitiv_Node *primitiv_node_func_const_div_node(float k, const primitiv_Node *x) {
   Node y = k / *to_cc(x);
   return to_c_from_value(y);
 }
 
-primitiv_Node *primitiv_func_node_div_node(const primitiv_Node *a, const primitiv_Node *b) {
+primitiv_Node *primitiv_node_func_node_div_node(const primitiv_Node *a, const primitiv_Node *b) {
   Node y = *to_cc(a) / *to_cc(b);
   return to_c_from_value(y);
 }
 
-primitiv_Tensor *primitiv_func_tensor_add_const(const primitiv_Tensor *x, float k) {
+primitiv_Tensor *primitiv_tensor_func_tensor_add_const(const primitiv_Tensor *x, float k) {
   Tensor y = *to_cc(x) + k;
   return to_c_from_value(y);
 }
 
-primitiv_Tensor *primitiv_func_const_add_tensor(float k, const primitiv_Tensor *x) {
+primitiv_Tensor *primitiv_tensor_func_const_add_tensor(float k, const primitiv_Tensor *x) {
   Tensor y = k + *to_cc(x);
   return to_c_from_value(y);
 }
 
-primitiv_Tensor *primitiv_func_tensor_add_tensor(const primitiv_Tensor *a, const primitiv_Tensor *b) {
+primitiv_Tensor *primitiv_tensor_func_tensor_add_tensor(const primitiv_Tensor *a, const primitiv_Tensor *b) {
   Tensor y = *to_cc(a) + *to_cc(b);
   return to_c_from_value(y);
 }
 
-primitiv_Tensor *primitiv_func_tensor_sub_const(const primitiv_Tensor *x, float k) {
+primitiv_Tensor *primitiv_tensor_func_tensor_sub_const(const primitiv_Tensor *x, float k) {
   Tensor y = *to_cc(x) - k;
   return to_c_from_value(y);
 }
 
-primitiv_Tensor *primitiv_func_const_sub_tensor(float k, const primitiv_Tensor *x) {
+primitiv_Tensor *primitiv_tensor_func_const_sub_tensor(float k, const primitiv_Tensor *x) {
   Tensor y = k - *to_cc(x);
   return to_c_from_value(y);
 }
 
-primitiv_Tensor *primitiv_func_tensor_sub_tensor(const primitiv_Tensor *a, const primitiv_Tensor *b) {
+primitiv_Tensor *primitiv_tensor_func_tensor_sub_tensor(const primitiv_Tensor *a, const primitiv_Tensor *b) {
   Tensor y = *to_cc(a) - *to_cc(b);
   return to_c_from_value(y);
 }
 
-primitiv_Tensor *primitiv_func_tensor_mul_const(const primitiv_Tensor *x, float k) {
+primitiv_Tensor *primitiv_tensor_func_tensor_mul_const(const primitiv_Tensor *x, float k) {
   Tensor y = *to_cc(x) * k;
   return to_c_from_value(y);
 }
 
-primitiv_Tensor *primitiv_func_const_mul_tensor(float k, const primitiv_Tensor *x) {
+primitiv_Tensor *primitiv_tensor_func_const_mul_tensor(float k, const primitiv_Tensor *x) {
   Tensor y = k * *to_cc(x);
   return to_c_from_value(y);
 }
 
-primitiv_Tensor *primitiv_func_tensor_mul_tensor(const primitiv_Tensor *a, const primitiv_Tensor *b) {
+primitiv_Tensor *primitiv_tensor_func_tensor_mul_tensor(const primitiv_Tensor *a, const primitiv_Tensor *b) {
   Tensor y = *to_cc(a) * *to_cc(b);
   return to_c_from_value(y);
 }
 
-primitiv_Tensor *primitiv_func_tensor_div_const(const primitiv_Tensor *x, float k) {
+primitiv_Tensor *primitiv_tensor_func_tensor_div_const(const primitiv_Tensor *x, float k) {
   Tensor y = *to_cc(x) / k;
   return to_c_from_value(y);
 }
 
-primitiv_Tensor *primitiv_func_const_div_tensor(float k, const primitiv_Tensor *x) {
+primitiv_Tensor *primitiv_tensor_func_const_div_tensor(float k, const primitiv_Tensor *x) {
   Tensor y = k / *to_cc(x);
   return to_c_from_value(y);
 }
 
-primitiv_Tensor *primitiv_func_tensor_div_tensor(const primitiv_Tensor *a, const primitiv_Tensor *b) {
+primitiv_Tensor *primitiv_tensor_func_tensor_div_tensor(const primitiv_Tensor *a, const primitiv_Tensor *b) {
   Tensor y = *to_cc(a) / *to_cc(b);
   return to_c_from_value(y);
 }
--- primitiv_c/functions.h
+++ primitiv_c/functions.h
@@ -12,57 +12,53 @@
 extern "C" {
 #endif
 
-primitiv_Node *primitiv_func_node_add_const(const primitiv_Node *x, float k);
+primitiv_Node *primitiv_node_func_node_add_const(const primitiv_Node *x, float k);
 
-primitiv_Node *primitiv_func_const_add_node(float k, const primitiv_Node *x);
+primitiv_Node *primitiv_node_func_const_add_node(float k, const primitiv_Node *x);
 
-primitiv_Node *primitiv_func_node_add_node(const primitiv_Node *a, const primitiv_Node *b);
+primitiv_Node *primitiv_node_func_node_add_node(const primitiv_Node *a, const primitiv_Node *b);
 
-primitiv_Node *primitiv_func_node_sub_const(const primitiv_Node *x, float k);
+primitiv_Node *primitiv_node_func_node_sub_const(const primitiv_Node *x, float k);
 
-primitiv_Node *primitiv_func_const_sub_node(float k, const primitiv_Node *x);
+primitiv_Node *primitiv_node_func_const_sub_node(float k, const primitiv_Node *x);
 
-primitiv_Node *primitiv_func_node_sub_node(const primitiv_Node *a, const primitiv_Node *b);
+primitiv_Node *primitiv_node_func_node_sub_node(const primitiv_Node *a, const primitiv_Node *b);
 
-primitiv_Node *primitiv_func_node_mul_const(const primitiv_Node *x, float k);
+primitiv_Node *primitiv_node_func_node_mul_const(const primitiv_Node *x, float k);
 
-primitiv_Node *primitiv_func_const_mul_node(float k, const primitiv_Node *x);
+primitiv_Node *primitiv_node_func_const_mul_node(float k, const primitiv_Node *x);
 
-primitiv_Node *primitiv_func_node_mul_node(const primitiv_Node *a, const primitiv_Node *b);
+primitiv_Node *primitiv_node_func_node_mul_node(const primitiv_Node *a, const primitiv_Node *b);
 
-primitiv_Node *primitiv_func_node_div_const(const primitiv_Node *x, float k);
+primitiv_Node *primitiv_node_func_node_div_const(const primitiv_Node *x, float k);
 
-primitiv_Node *primitiv_func_const_div_node(float k, const primitiv_Node *x);
+primitiv_Node *primitiv_node_func_const_div_node(float k, const primitiv_Node *x);
 
-primitiv_Node *primitiv_func_node_div_node(const primitiv_Node *a, const primitiv_Node *b);
+primitiv_Node *primitiv_node_func_node_div_node(const primitiv_Node *a, const primitiv_Node *b);
 
-primitiv_Tensor *primitiv_func_tensor_add_const(const primitiv_Tensor *x, float k);
+primitiv_Tensor *primitiv_tensor_func_tensor_add_const(const primitiv_Tensor *x, float k);
 
-primitiv_Tensor *primitiv_func_const_add_tensor(float k, const primitiv_Tensor *x);
+primitiv_Tensor *primitiv_tensor_func_const_add_tensor(float k, const primitiv_Tensor *x);
 
-primitiv_Tensor *primitiv_func_tensor_add_tensor(const primitiv_Tensor *a, const primitiv_Tensor *b);
+primitiv_Tensor *primitiv_tensor_func_tensor_add_tensor(const primitiv_Tensor *a, const primitiv_Tensor *b);
 
-primitiv_Tensor *primitiv_func_tensor_sub_const(const primitiv_Tensor *x, float k);
+primitiv_Tensor *primitiv_tensor_func_tensor_sub_const(const primitiv_Tensor *x, float k);
 
-primitiv_Tensor *primitiv_func_const_sub_tensor(float k, const primitiv_Tensor *x);
+primitiv_Tensor *primitiv_tensor_func_const_sub_tensor(float k, const primitiv_Tensor *x);
 
-primitiv_Tensor *primitiv_func_tensor_sub_tensor(const primitiv_Tensor *a, const primitiv_Tensor *b);
+primitiv_Tensor *primitiv_tensor_func_tensor_sub_tensor(const primitiv_Tensor *a, const primitiv_Tensor *b);
 
-primitiv_Tensor *primitiv_func_tensor_mul_const(const primitiv_Tensor *x, float k);
+primitiv_Tensor *primitiv_tensor_func_tensor_mul_const(const primitiv_Tensor *x, float k);
 
-primitiv_Tensor *primitiv_func_const_mul_tensor(float k, const primitiv_Tensor *x);
+primitiv_Tensor *primitiv_tensor_func_const_mul_tensor(float k, const primitiv_Tensor *x);
 
-primitiv_Tensor *primitiv_func_tensor_mul_tensor(const primitiv_Tensor *a, const primitiv_Tensor *b);
+primitiv_Tensor *primitiv_tensor_func_tensor_mul_tensor(const primitiv_Tensor *a, const primitiv_Tensor *b);
 
-primitiv_Tensor *primitiv_func_tensor_div_const(const primitiv_Tensor *x, float k);
+primitiv_Tensor *primitiv_tensor_func_tensor_div_const(const primitiv_Tensor *x, float k);
 
-primitiv_Tensor *primitiv_func_const_div_tensor(float k, const primitiv_Tensor *x);
+primitiv_Tensor *primitiv_tensor_func_const_div_tensor(float k, const primitiv_Tensor *x);
 
-primitiv_Tensor *primitiv_func_tensor_div_tensor(const primitiv_Tensor *a, const primitiv_Tensor *b);
-
-primitiv_Node *primitiv_node_func_mean(const primitiv_Node *x, uint32_t dim);
-
-primitiv_Tensor *primitiv_tensor_func_mean(const primitiv_Tensor *x, uint32_t dim);
+primitiv_Tensor *primitiv_tensor_func_tensor_div_tensor(const primitiv_Tensor *a, const primitiv_Tensor *b);
 
 primitiv_Node *primitiv_node_func_mean(const primitiv_Node *x, uint32_t dim);
 
