d2cf8a3bd3c66341a863d8208e847d514b640538
odashi <yus.takara@gmail.com>
2017-12-15 17:00:37 +0900

    Fix FindPrimitiv.cmake.

--- cmake/FindPrimitiv.cmake
+++ cmake/FindPrimitiv.cmake
@@ -6,6 +6,10 @@
 #   PRIMITIV_INCLUDE_DIR - primitiv include directory.
 #   PRIMITIV_LIBRARIES - primitiv library files.
 #
+#   PRIMITIV_C_FOUND - Whether or not the system has the primitiv C API.
+#   PRIMITIV_C_INCLUDE_DIR - primitiv C API include directory.
+#   PRIMITIV_C_LIBRARIES - primitiv C API library files.
+#
 # This script reads hints about search locations from the following
 # environment variables:
 #
@@ -14,11 +18,16 @@
 #
 # Author: Yusuke Oda (odashi) <yus.takara@gmail.com>
 # Created: 2017/08/13
-# Updated: 2017/08/13
+# Updated: 2017/12/15
 #
-# This script is provided as a part of the primitiv library.
+# This script is provided as a part of the primitiv core library.
 # Redistributing and using this script is allowed according to
 # the Apache License Version 2.
+
+
+#
+# Finding primitiv core library
+#
 
 find_path(PRIMITIV_INCLUDE_DIR NAMES primitiv/primitiv.h
   HINTS
@@ -45,3 +54,35 @@ else()
   message(STATUS "Primitiv is not installed.")
   set(PRIMITIV_FOUND OFF)
 endif()
+
+
+#
+# Finding primitiv C API
+#
+
+find_path(PRIMITIV_C_INCLUDE_DIR NAMES primitiv/c/api.h
+  HINTS
+  ENV PRIMITIV_ROOT
+  ENV PRIMITIV_ROOT_DIR
+  PATHS
+  ${CMAKE_INSTALL_PREFIX}/include
+)
+
+find_library(PRIMITIV_C_LIBRARIES NAMES primitiv_c
+  HINTS
+  ENV PRIMITIV_ROOT
+  ENV PRIMITIV_ROOT_DIR
+  PATHS
+  ${CMAKE_INSTALL_PREFIX}/lib
+)
+
+if (PRIMITIV_C_INCLUDE_DIR AND PRIMITIV_C_LIBRARIES)
+  message("-- Found Primitiv C API: " ${PRIMITIV_C_LIBRARIES})
+  #message("-- Primitiv C API include dir: " ${PRIMITIV_C_INCLUDE_DIR})
+  #message("-- Primitiv C API libraries: " ${PRIMITIV_C_LIBRARIES})
+  set(PRIMITIV_C_FOUND ON)
+else()
+  message(STATUS "Primitiv C API is not installed.")
+  set(PRIMITIV_C_FOUND OFF)
+endif()
+
