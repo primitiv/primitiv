b629d5f8b2189faa87b08b5cdb74e7a775c0eba5
Yusuke Oda <yus.takara@gmail.com>
2017-12-16 05:43:25 +0900

    Make FindPrimitiv.cmake to check the header version.

--- cmake/FindPrimitiv.cmake
+++ cmake/FindPrimitiv.cmake
@@ -10,6 +10,10 @@
 #   PRIMITIV_C_INCLUDE_DIR - primitiv C API include directory.
 #   PRIMITIV_C_LIBRARIES - primitiv C API library files.
 #
+#   PRIMITIV_VERSION - Header version of the installed library.
+#   PRIMITIV_VERSION_OK - `ON` if the installed header satisfies version
+#                         requirement, `OFF` otherwise.
+#
 # This script reads hints about search locations from the following
 # environment variables:
 #
@@ -26,7 +30,7 @@
 
 
 #
-# Finding primitiv core library
+# Finds primitiv core library
 #
 
 find_path(PRIMITIV_INCLUDE_DIR NAMES primitiv/primitiv.h
@@ -46,18 +50,18 @@ find_library(PRIMITIV_LIBRARIES NAMES primitiv
 )
 
 if (PRIMITIV_INCLUDE_DIR AND PRIMITIV_LIBRARIES)
-  message("-- Found Primitiv: " ${PRIMITIV_LIBRARIES})
-  #message("-- Primitiv include dir: " ${PRIMITIV_INCLUDE_DIR})
-  #message("-- Primitiv libraries: " ${PRIMITIV_LIBRARIES})
+  message("-- Found primitiv: " ${PRIMITIV_LIBRARIES})
+  #message("-- primitiv include dir: " ${PRIMITIV_INCLUDE_DIR})
+  #message("-- primitiv libraries: " ${PRIMITIV_LIBRARIES})
   set(PRIMITIV_FOUND ON)
 else()
-  message(STATUS "Primitiv is not installed.")
+  message(STATUS "primitiv is not installed.")
   set(PRIMITIV_FOUND OFF)
 endif()
 
 
 #
-# Finding primitiv C API
+# Finds primitiv C API
 #
 
 find_path(PRIMITIV_C_INCLUDE_DIR NAMES primitiv/c/api.h
@@ -77,12 +81,58 @@ find_library(PRIMITIV_C_LIBRARIES NAMES primitiv_c
 )
 
 if (PRIMITIV_C_INCLUDE_DIR AND PRIMITIV_C_LIBRARIES)
-  message("-- Found Primitiv C API: " ${PRIMITIV_C_LIBRARIES})
-  #message("-- Primitiv C API include dir: " ${PRIMITIV_C_INCLUDE_DIR})
-  #message("-- Primitiv C API libraries: " ${PRIMITIV_C_LIBRARIES})
+  message("-- Found primitiv C API: " ${PRIMITIV_C_LIBRARIES})
+  #message("-- primitiv C API include dir: " ${PRIMITIV_C_INCLUDE_DIR})
+  #message("-- primitiv C API libraries: " ${PRIMITIV_C_LIBRARIES})
   set(PRIMITIV_C_FOUND ON)
 else()
   message(STATUS "Primitiv C API is not installed.")
   set(PRIMITIV_C_FOUND OFF)
 endif()
 
+
+#
+# Checks version
+#
+
+if(NOT Primitiv_FIND_VERSION)
+  set(Primitiv_FIND_VERSION "${Primitiv_FIND_VERSION_MAJOR}.${Primitiv_FIND_VERSION_MINOR}.${Primitiv_FIND_VERSION_PATCH}")
+endif()
+
+file(READ
+  "${PRIMITIV_INCLUDE_DIR}/primitiv/version.h"
+  _primitiv_version_header
+)
+
+string(REGEX MATCH
+  "#[ \t]*define[ \t]+PRIMITIV_VERSION_MAJOR[ \t]+([0-9]+)"
+  _primitiv_version_match "${_primitiv_version_header}"
+)
+set(PRIMITIV_VERSION_MAJOR "${CMAKE_MATCH_1}")
+
+string(REGEX MATCH
+  "#[ \t]*define[ \t]+PRIMITIV_VERSION_MINOR[ \t]+([0-9]+)"
+  _primitiv_version_match "${_primitiv_version_header}"
+)
+set(PRIMITIV_VERSION_MINOR "${CMAKE_MATCH_1}")
+
+string(REGEX MATCH
+  "#[ \t]*define[ \t]+PRIMITIV_VERSION_PATCH[ \t]+([0-9]+)"
+  _primitiv_version_match "${_primitiv_version_header}"
+)
+set(PRIMITIV_VERSION_PATCH "${CMAKE_MATCH_1}")
+
+set(PRIMITIV_VERSION "${PRIMITIV_VERSION_MAJOR}.${PRIMITIV_VERSION_MINOR}.${PRIMITIV_VERSION_PATCH}")
+
+#message("primitiv version requested: ${PRIMITIV_FIND_VERSION}")
+#message("primitiv version found: ${PRIMITIV_VERSION}")
+
+if(${PRIMITIV_VERSION} VERSION_LESS ${Primitiv_FIND_VERSION})
+  message(STATUS
+    "primitiv version ${PRIMITIV_VERSION} found in ${PRIMITIV_INCLUDE_DIR}, "
+    "but at least version ${Primitiv_FIND_VERSION} is required."
+  )
+  set(PRIMITIV_VERSION_OK OFF)
+else()
+  set(PRIMITIV_VERSION_OK ON)
+endif()
