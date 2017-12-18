960612effd26db064817062aa085596c9f08b2fd
Yusuke Oda <yus.takara@gmail.com>
2017-12-18 09:43:20 +0900

    Implement matmul function (it works about 20 times faster than Naive).

--- primitiv/eigen_device.cc
+++ primitiv/eigen_device.cc
@@ -5,6 +5,9 @@
 #include <cstring>
 #include <cmath>
 #include <iostream>
+
+#include <Eigen/Eigen>
+
 #include <primitiv/eigen_device.h>
 #include <primitiv/error.h>
 
@@ -500,45 +503,36 @@ void Eigen::transpose_fw_impl(const Tensor &x, Tensor &y) {
 }
 
 void Eigen::matmul_fw_impl(const Tensor &a, const Tensor &b, Tensor &y) {
-  const std::uint32_t d1 = a.shape()[0];
-  const std::uint32_t d2 = a.shape()[1];
-  const std::uint32_t d3 = b.shape()[1];
-  const std::uint32_t bs = y.shape().batch();
-  const std::uint32_t dest_shift = d1 * d3;
-  const std::uint32_t src_a_shift = a.shape().has_batch() * d1 * d2;
-  const std::uint32_t src_b_shift = b.shape().has_batch() * d2 * d3;
+  using ::Eigen::Map;
+  using ::Eigen::MatrixXf;
 
-  float *dest = MDATA(y);
+  const std::uint32_t di = a.shape()[0];
+  const std::uint32_t dj = a.shape()[1];
+  const std::uint32_t dk = b.shape()[1];
+
   const float *src_a = CDATA(a);
   const float *src_b = CDATA(b);
+  float *dest = MDATA(y);
 
-  for (std::uint32_t batch = 0; batch < bs; ++batch) {
-    for (std::uint32_t n = 0; n < dest_shift; ++n) {
-      dest[n] = 0;
+  if (a.shape().has_batch()) {
+    // Do multiplication multiple times.
+    const std::uint32_t a_skip = di * dj;
+    const std::uint32_t b_skip = b.shape().has_batch() * dj * dk;
+    const std::uint32_t y_skip = di * dk;
+    const std::uint32_t bs = a.shape().batch();
+    for (std::uint32_t n = 0; n < bs; ++n) {
+      Map<const MatrixXf> aa(src_a + n * a_skip, di, dj);
+      Map<const MatrixXf> bb(src_b + n * b_skip, dj, dk);
+      Map<MatrixXf> yy(dest + n * y_skip, di, dk);
+      yy.noalias() = aa * bb;
     }
-    for (std::uint32_t k = 0; k < d3; k += 8) {
-      const std::uint32_t ek = std::min(k + 8, d3);
-      for (std::uint32_t i = 0; i < d1; i += 8) {
-        const std::uint32_t ei = std::min(i + 8, d1);
-        for (std::uint32_t j = 0; j < d2; j += 8) {
-          const std::uint32_t ej = std::min(j + 8, d2);
-          for (std::uint32_t kk = k; kk < ek; ++kk) {
-            const std::uint32_t kk_d1 = kk * d1;
-            const std::uint32_t kk_d2 = kk * d2;
-            for (std::uint32_t ii = i; ii < ei; ++ii) {
-              float tmp = 0;
-              for (std::uint32_t jj = j; jj < ej; ++jj) {
-                tmp += src_a[ii + jj * d1] * src_b[jj + kk_d2];
-              }
-              dest[ii + kk_d1] += tmp;
-            }
-          }
-        }
-      }
-    }
-    dest += dest_shift;
-    src_a += src_a_shift;
-    src_b += src_b_shift;
+  } else {
+    // Do multiplication only once using a combined matrix.
+    const std::uint32_t dk_batch = dk * b.shape().batch();
+    Map<const MatrixXf> aa(src_a, di, dj);
+    Map<const MatrixXf> bb(src_b, dj, dk_batch);
+    Map<MatrixXf> yy(dest, di, dk_batch);
+    yy.noalias() = aa * bb;
   }
 }
 
@@ -551,9 +545,45 @@ void Eigen::transpose_bw_impl(
 void Eigen::matmul_bw_impl(
     const Tensor &a, const Tensor &b, const Tensor &, const Tensor &gy,
     Tensor &ga, Tensor &gb) {
-  // TODO(odashi): This code could be slow and requires memory. Fix this.
-  inplace_add_impl(matmul_fw(gy, transpose_fw(b)), ga);
-  inplace_add_impl(matmul_fw(transpose_fw(a), gy), gb);
+  using ::Eigen::Map;
+  using ::Eigen::MatrixXf;
+
+  const std::uint32_t di = a.shape()[0];
+  const std::uint32_t dj = a.shape()[1];
+  const std::uint32_t dk = b.shape()[1];
+
+  const float *src_a = CDATA(a);
+  const float *src_b = CDATA(b);
+  const float *src_gy = CDATA(gy);
+  float *dest_ga = MDATA(ga);
+  float *dest_gb = MDATA(gb);
+
+  if (a.shape().has_batch()) {
+    // Do multiplication multiple times.
+    const std::uint32_t a_skip = di * dj;
+    const std::uint32_t b_skip = b.shape().has_batch() * dj * dk;
+    const std::uint32_t y_skip = di * dk;
+    const std::uint32_t bs = a.shape().batch();
+    for (std::uint32_t n = 0; n < bs; ++n) {
+      Map<const MatrixXf> aa(src_a + n * a_skip, di, dj);
+      Map<const MatrixXf> bb(src_b + n * b_skip, dj, dk);
+      Map<const MatrixXf> gyy(src_gy + n * y_skip, di, dk);
+      Map<MatrixXf> gaa(dest_ga + n * a_skip, di, dj);
+      Map<MatrixXf> gbb(dest_gb + n * b_skip, dj, dk);
+      gaa.noalias() += gyy * bb.transpose();
+      gbb.noalias() += aa.transpose() * gyy;
+    }
+  } else {
+    // Do multiplication only once using a combined matrix.
+    const std::uint32_t dk_batch = dk * b.shape().batch();
+    Map<const MatrixXf> aa(src_a, di, dj);
+    Map<const MatrixXf> bb(src_b, dj, dk_batch);
+    Map<const MatrixXf> gyy(src_gy, di, dk_batch);
+    Map<MatrixXf> gaa(dest_ga, di, dj);
+    Map<MatrixXf> gbb(dest_gb, dj, dk_batch);
+    gaa.noalias() += gyy * bb.transpose();
+    gbb.noalias() += aa.transpose() * gyy;
+  }
 }
 
 void Eigen::sum_fw_impl(const Tensor &x, std::uint32_t dim, Tensor &y) {
