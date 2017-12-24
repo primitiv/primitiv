a943849a214756097f98080e295b71231ddfc44d
Koichi Akabe <vbkaisetsu@gmail.com>
2017-12-24 13:37:33 +0900

    Use Eigen without installing

--- .travis/ubuntu-vanilla.sh
+++ .travis/ubuntu-vanilla.sh
@@ -5,23 +5,19 @@ set -xe
 sudo apt update
 sudo apt install -y build-essential cmake3 libgtest-dev
 
-# install Eigen
+# download Eigen
 #
 # NOTE(vbkaisetsu):
 # Ubuntu 14.04 contains Eigen 3.2
 # primitiv requires 3.3 or later
 #
 wget http://bitbucket.org/eigen/eigen/get/3.3.4.tar.bz2 -O ./eigen.tar.bz2
-mkdir ./eigen
-tar xf ./eigen.tar.bz2 -C ./eigen --strip-components 1
-mkdir ./eigen/build
-cd ./eigen/build
-cmake ..
-make && sudo make install
+mkdir $TRAVIS_BUILD_DIR/eigen
+tar xf ./eigen.tar.bz2 -C $TRAVIS_BUILD_DIR/eigen --strip-components 1
 
 # script
 cd $TRAVIS_BUILD_DIR
-cmake . -DPRIMITIV_USE_EIGEN=ON -DPRIMITIV_BUILD_TESTS=ON -DPRIMITIV_GTEST_SOURCE_DIR=/usr/src/gtest
+cmake . -DPRIMITIV_USE_EIGEN=ON -DEIGEN3_INCLUDE_DIR=$TRAVIS_BUILD_DIR/eigen -DPRIMITIV_BUILD_TESTS=ON -DPRIMITIV_GTEST_SOURCE_DIR=/usr/src/gtest
 make VERBOSE=1
 make test ARGS='-V'
 sudo make install
