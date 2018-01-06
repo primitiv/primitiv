6cb4b11ac34d2bf6fb9b78de1d57a6cc70505cd9
Koichi Akabe <vbkaisetsu@gmail.com>
2018-01-06 19:18:12 +0900

    Add string_utils::to_string() for Android that does not support std::to_string()

--- primitiv/string_utils.h
+++ primitiv/string_utils.h
@@ -2,9 +2,11 @@
 #define PRIMITIV_STRING_UTILS_H_
 
 #include <cstdint>
+#include <cstdlib>
 #include <sstream>
 #include <string>
 #include <vector>
+#include <primitiv/error.h>
 
 namespace primitiv {
 namespace string_utils {
@@ -18,7 +20,7 @@ namespace string_utils {
  *           1 content         : strs[0]
  *           2 contents or more: strs[0] + delim + strs[1] + delim + ...
  */
-std::string join(
+inline std::string join(
     const std::vector<std::string> &strs,
     const std::string &delim) {
   if (strs.empty()) return std::string();
@@ -27,6 +29,36 @@ std::string join(
   for (std::uint32_t i = 1; i < strs.size(); ++i) ss << delim << strs[i];
   return ss.str();
 }
+
+/**
+ * Wrapper functions of std::to_string()
+ */
+#ifdef __ANDROID__
+inline std::string to_string(std::uint32_t value) {
+  char *c_str = nullptr;
+  if (asprintf(&c_str, "%d", value) < 0) {
+    THROW_ERROR("Failed to format value");
+  }
+  std::unique_ptr<char, decltype(&std::free)> u(c_str, std::free);
+  std::string s(u.get());
+  return s;
+}
+
+inline std::string to_string(float value) {
+  char *c_str = nullptr;
+  if (asprintf(&c_str, "%f", value) < 0) {
+    THROW_ERROR("Failed to format value");
+  }
+  std::unique_ptr<char, decltype(&std::free)> u(c_str, std::free);
+  std::string s(u.get());
+  return s;
+}
+#else
+template<typename T>
+inline std::string to_string(T value) {
+  return std::to_string(value);
+}
+#endif
 
 }  // namespace string_utils
 }  // namespace primitiv
