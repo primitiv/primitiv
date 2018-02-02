21f1a4dce887d83e99bbcdf6c06d3649d8cd5a2f
odashi <yus.takara@gmail.com>
2018-02-02 13:22:02 +0900

    Separate all Naive device functions.

---
+++ primitiv/device_ops/softplus_naive.cc
@@ -0,0 +1,18 @@
+#include <primitiv/config.h>
+
+#include <cmath>
+
+#include <primitiv/naive_device.h>
+#include <primitiv/device_ops/naive_utils.h>
+
+namespace primitiv {
+namespace devices {
+
+CPUDEV_FW_X(
+    softplus, src[i] > 0
+      ? src[i] + std::log(1 + std::exp(-src[i]))
+      : std::log(1 + std::exp(src[i])));
+CPUDEV_BW_X(softplus, (.5 + .5 * std::tanh(.5 * px[i])) * pgy[i]);
+
+}  // namespace devices
+}  // namespace primitiv
