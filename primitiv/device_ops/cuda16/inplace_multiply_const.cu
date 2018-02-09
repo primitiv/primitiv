7df609a361fd4ebba1e8dadfe99ae7f3f124c1c5
odashi <yus.takara@gmail.com>
2018-02-09 18:30:42 +0900

    Implement multiply_const.

--- primitiv/device_ops/cuda16/inplace_multiply_const.cu
+++ primitiv/device_ops/cuda16/inplace_multiply_const.cu
@@ -5,25 +5,24 @@
 #include <primitiv/device_ops/cuda16/common.h>
 
 namespace {
-/*
+
 __global__ void inplace_multiply_const_dev(
-    float k, std::uint32_t size, float *px) {
+    float k, std::uint32_t size, half *px) {
   const std::uint32_t i = IDX;
-  if (i < size) px[i] *= k;
+  if (i < size) px[i] = __float2half(__half2float(px[i]) * k);
 }
-*/
+
 }  // namespace
 
 namespace primitiv {
 namespace devices {
 
 void CUDA16::inplace_multiply_const_impl(float k, Tensor &x) {
-  THROW_NOT_IMPLEMENTED;
-  /*const std::uint32_t size = x.shape().size();
+  const std::uint32_t size = x.shape().size();
   const std::uint32_t g1 = GRID_SIZE(size, dim1_x_);
   CUDA_CALL(::cudaSetDevice(dev_id_));
-  ::inplace_multiply_const_dev<<<g1, dim1_x_>>>(k, size, MDATA(float, x));
-*/}
+  ::inplace_multiply_const_dev<<<g1, dim1_x_>>>(k, size, MDATA(half, x));
+}
 
 }  // namespace devices
 }  // namespace primitiv
