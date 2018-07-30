6f43b0cc49c643f7cac019b198373b38f7b3d51a
Yusuke Oda <oday@google.com>
2018-07-30 16:19:50 +0900

    Remove duplicated definitions.

--- primitiv/core/mixins/identifiable.h
+++ primitiv/core/mixins/identifiable.h
@@ -1,42 +1,15 @@
-#ifndef PRIMITIV_CORE_MIXINS_H_
-#define PRIMITIV_CORE_MIXINS_H_
+#ifndef PRIMITIV_CORE_MIXINS_IDENTIFIABLE_H_
+#define PRIMITIV_CORE_MIXINS_IDENTIFIABLE_H_
 
 #include <cstdint>
 #include <mutex>
 #include <unordered_map>
 
 #include <primitiv/core/error.h>
+#include <primitiv/mixins/nonmovable.h>
 
 namespace primitiv {
 namespace mixins {
-
-/**
- * Mix-in class to prohibit copying.
- */
-template<typename T>
-class Noncopyable {
-private:
-  Noncopyable(const Noncopyable &) = delete;
-  Noncopyable &operator=(const Noncopyable &) = delete;
-protected:
-  Noncopyable() = default;
-  ~Noncopyable() = default;
-};
-
-/**
- * Mix-in class to prohibit moving and copying.
- */
-template<typename T>
-class Nonmovable {
-private:
-  Nonmovable(const Nonmovable &) = delete;
-  Nonmovable(Nonmovable &&) = delete;
-  Nonmovable &operator=(const Nonmovable &) = delete;
-  Nonmovable &operator=(Nonmovable &&) = delete;
-protected:
-  Nonmovable() = default;
-  ~Nonmovable() = default;
-};
 
 /**
  * Mix-in class to allow to bind object IDs.
@@ -87,68 +60,7 @@ std::unordered_map<std::uint64_t, T *> Identifiable<T>::objects_;
 template<typename T>
 std::mutex Identifiable<T>::mutex_;
 
-/**
- * Mix-in class to provide default value setter/getter.
- */
-template<typename T>
-class DefaultSettable {
-  DefaultSettable(const DefaultSettable &) = delete;
-  DefaultSettable(DefaultSettable &&) = delete;
-  DefaultSettable &operator=(const DefaultSettable &) = delete;
-  DefaultSettable &operator=(DefaultSettable &&) = delete;
-
-  /**
-   * Pointer of current default object.
-   */
-  static T *default_obj_;
-
-protected:
-  DefaultSettable() = default;
-
-  ~DefaultSettable() {
-    // If the current default object is this, unregister it.
-    if (default_obj_ == static_cast<T *>(this)) {
-      default_obj_ = nullptr;
-    }
-  }
-
-public:
-  /**
-   * Retrieves the current default object.
-   * @return Reference of the current default object.
-   * @throw primitiv::Error Default object is null.
-   */
-  static T &get_default() {
-    if (!default_obj_) PRIMITIV_THROW_ERROR("Default object is null.");
-    return *default_obj_;
-  }
-
-  /**
-   * Specifies a new default object.
-   * @param obj Reference of the new default object.
-   */
-  static void set_default(T &obj) {
-    default_obj_ = &obj;
-  }
-
-  /**
-   * Obtains the reference of the object pointed by a pointer, or obtains the
-   * default object.
-   * @param ptr Pointer of an object, or `nullptr`.
-   * @return Reference of the object pointed by `ptr`,
-   *         or reference of the default object if `ptr` is `nullptr`.
-   * @throw primitiv::Error `nullptr` is given by `ptr` although the default
-   *                        object is also null.
-   */
-  static T &get_reference_or_default(T *ptr) {
-    return ptr ? *ptr : get_default();
-  }
-};
-
-template<typename T>
-T *DefaultSettable<T>::default_obj_ = nullptr;
-
 }  // namespace mixins
 }  // namespace primitiv
 
-#endif  // PRIMITIV_CORE_MIXINS_H_
+#endif  // PRIMITIV_CORE_MIXINS_IDENTIFIABLE_H_
