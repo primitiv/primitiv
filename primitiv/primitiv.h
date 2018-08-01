144f8d66b3f2c4527b674cb16ad10b60fc984ca0
Koichi Akabe <vbkaisetsu@gmail.com>
2018-08-01 13:45:56 +0900

    Fix includes in primitiv.h

--- primitiv/primitiv.h
+++ primitiv/primitiv.h
@@ -10,22 +10,22 @@
 #include <primitiv/core/graph.h>
 #include <primitiv/core/initializer_impl.h>
 #include <primitiv/core/model.h>
-#include <primitiv/core/naive_device.h>
 #include <primitiv/core/parameter.h>
 #include <primitiv/core/shape.h>
 #include <primitiv/core/tensor.h>
 #include <primitiv/core/optimizer_impl.h>
+#include <primitiv/devices/naive/device.h>
 
 // Header files for specific device classes.
 #ifdef PRIMITIV_USE_EIGEN
-#include <primitiv/core/eigen_device.h>
+#include <primitiv/devices/eigen/device.h>
 #endif  // PRIMITIV_USE_EIGEN
 #ifdef PRIMITIV_USE_CUDA
-#include <primitiv/core/cuda_device.h>
-#include <primitiv/core/cuda16_device.h>
+#include <primitiv/devices/cuda/device.h>
+#include <primitiv/devices/cuda16/device.h>
 #endif  // PRIMITIV_USE_CUDA
 #ifdef PRIMITIV_USE_OPENCL
-#include <primitiv/core/opencl_device.h>
+#include <primitiv/devices/opencl/device.h>
 #endif  // PRIMITIV_USE_OPENCL
 
 #endif  // PRIMITIV_PRIMITIV_H_
