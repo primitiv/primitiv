8010d44f02205ed91ab6476cabf245ec2f1a83fd
Koichi Akabe <vbkaisetsu@gmail.com>
2018-08-07 00:15:26 +0900

    Add C API for permute_dims()

--- primitiv/c/functions.h
+++ primitiv/c/functions.h
@@ -100,6 +100,13 @@ PRIMITIV_C_API PRIMITIV_C_STATUS primitivApplyTensorReshape(
 PRIMITIV_C_DECL_UNARY_FUNC(Flatten);
 PRIMITIV_C_DECL_UNARY_FUNC(Transpose);
 
+PRIMITIV_C_API PRIMITIV_C_STATUS primitivApplyNodePermuteDims(
+    const primitivNode_t *x, const uint32_t *perm, size_t n,
+    primitivNode_t **y);
+PRIMITIV_C_API PRIMITIV_C_STATUS primitivApplyTensorPermuteDims(
+    const primitivTensor_t *x, const uint32_t *perm, size_t n,
+    primitivTensor_t **y);
+
 PRIMITIV_C_API PRIMITIV_C_STATUS primitivApplyNodeMatmul(
     const primitivNode_t *a, const primitivNode_t *b, primitivNode_t **y);
 PRIMITIV_C_API PRIMITIV_C_STATUS primitivApplyTensorMatmul(
