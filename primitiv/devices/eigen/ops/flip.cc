b39c225cd6ae176fa4397fdcec91404d2e1623ab
Koichi Akabe <vbkaisetsu@gmail.com>
2018-08-07 23:48:47 +0900

    Refactoring

--- primitiv/devices/eigen/ops/flip.cc
+++ primitiv/devices/eigen/ops/flip.cc
@@ -7,37 +7,32 @@ namespace primitiv {
 namespace devices {
 
 void Eigen::flip_fw_impl(const Tensor &x, std::uint32_t dim, Tensor &y) {
+  const float *px = CDATA(x);
+  float *py = MDATA(y);
   if (dim == 0) {
     const std::uint32_t di = x.shape()[0];
     const std::uint32_t dj = x.shape().volume() / di;
-    const float *src = CDATA(x);
-    float *dest = MDATA(y);
     const std::uint32_t skip = x.shape().volume();
     const std::uint32_t bs = x.shape().batch();
     for (std::uint32_t n = 0; n < bs; ++n) {
-      EMap<const EMatrixXf> xx(src + n * skip, di, dj);
-      EMap<EMatrixXf> yy(dest + n * skip, di, dj);
+      EMap<const EMatrixXf> xx(px + n * skip, di, dj);
+      EMap<EMatrixXf> yy(py + n * skip, di, dj);
       yy.noalias() = xx.colwise().reverse();
     }
   } else if (dim == x.shape().depth() - 1) {
     const std::uint32_t di = x.shape().lower_volume(dim);
     const std::uint32_t dj = x.shape()[dim];
-    const float *src = CDATA(x);
-    float *dest = MDATA(y);
     const std::uint32_t skip = x.shape().volume();
     const std::uint32_t bs = x.shape().batch();
     for (std::uint32_t n = 0; n < bs; ++n) {
-      EMap<const EMatrixXf> xx(src + n * skip, di, dj);
-      EMap<EMatrixXf> yy(dest + n * skip, di, dj);
+      EMap<const EMatrixXf> xx(px + n * skip, di, dj);
+      EMap<EMatrixXf> yy(py + n * skip, di, dj);
       yy.noalias() = xx.rowwise().reverse();
     }
   } else {
-    const Shape &s = x.shape();
-    const std::uint32_t n = s[dim];
-    const std::uint32_t skip = s.lower_volume(dim);
-    const std::uint32_t r = s.size() / n;
-    const float *px = CDATA(x);
-    float *py = MDATA(y);
+    const std::uint32_t n = x.shape()[dim];
+    const std::uint32_t skip = x.shape().lower_volume(dim);
+    const std::uint32_t r = x.shape().size() / n;
     for (std::uint32_t j = 0; j < n; ++j) {
       for (std::uint32_t i = 0; i < r; ++i) {
         const std::uint32_t offset = i * n - i % skip * (n - 1);
@@ -48,37 +43,32 @@ void Eigen::flip_fw_impl(const Tensor &x, std::uint32_t dim, Tensor &y) {
 }
 
 void Eigen::flip_bw_impl(const Tensor &gy, std::uint32_t dim, Tensor &gx) {
+  const float *pgy = CDATA(gy);
+  float *pgx = MDATA(gx);
   if (dim == 0) {
     const std::uint32_t di = gx.shape()[0];
     const std::uint32_t dj = gx.shape().volume() / di;
-    const float *src = CDATA(gy);
-    float *dest = MDATA(gx);
     const std::uint32_t skip = gx.shape().volume();
     const std::uint32_t bs = gx.shape().batch();
     for (std::uint32_t n = 0; n < bs; ++n) {
-      EMap<const EMatrixXf> gyy(src + n * skip, di, dj);
-      EMap<EMatrixXf> gxx(dest + n * skip, di, dj);
+      EMap<const EMatrixXf> gyy(pgy + n * skip, di, dj);
+      EMap<EMatrixXf> gxx(pgx + n * skip, di, dj);
       gxx.noalias() += gyy.colwise().reverse();
     }
   } else if (dim == gx.shape().depth() - 1) {
     const std::uint32_t di = gx.shape().lower_volume(dim);
     const std::uint32_t dj = gx.shape()[dim];
-    const float *src = CDATA(gy);
-    float *dest = MDATA(gx);
     const std::uint32_t skip = gx.shape().volume();
     const std::uint32_t bs = gx.shape().batch();
     for (std::uint32_t n = 0; n < bs; ++n) {
-      EMap<const EMatrixXf> gyy(src + n * skip, di, dj);
-      EMap<EMatrixXf> gxx(dest + n * skip, di, dj);
+      EMap<const EMatrixXf> gyy(pgy + n * skip, di, dj);
+      EMap<EMatrixXf> gxx(pgx + n * skip, di, dj);
       gxx.noalias() += gyy.rowwise().reverse();
     }
   } else {
-    const Shape &s = gx.shape();
-    const std::uint32_t n = s[dim];
-    const std::uint32_t skip = s.lower_volume(dim);
-    const std::uint32_t r = s.size() / n;
-    const float *pgy = CDATA(gy);
-    float *pgx = MDATA(gx);
+    const std::uint32_t n = gx.shape()[dim];
+    const std::uint32_t skip = gx.shape().lower_volume(dim);
+    const std::uint32_t r = gx.shape().size() / n;
     for (std::uint32_t j = 0; j < n; ++j) {
       for (std::uint32_t i = 0; i < r; ++i) {
         const std::uint32_t offset = i * n - i % skip * (n - 1);
