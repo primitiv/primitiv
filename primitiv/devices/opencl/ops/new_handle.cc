bcce22d8d252dceb18edb8c10c17185da1279b7f
Hiroki Teranishi <teranishihiroki@gmail.com>
2019-06-16 15:37:35 +0900

    Fix OpenCL::new_handle.

--- primitiv/devices/opencl/ops/new_handle.cc
+++ primitiv/devices/opencl/ops/new_handle.cc
@@ -5,8 +5,8 @@
 namespace primitiv {
 namespace devices {
 
-std::shared_ptr<void> OpenCL::new_handle(const Shape &shape) {
-  return state_->pool.allocate(sizeof(float) * shape.size());
+std::shared_ptr<void> OpenCL::new_handle(const Shape &shape, std::size_t * const allocated_size) {
+  return state_->pool.allocate(sizeof(float) * shape.size(), allocated_size);
 }
 
 }  // namespace devices
